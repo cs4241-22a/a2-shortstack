80a548b0e813346c51eb807e7131a2910587f800
Alex Bolduc <aebolduc@wpi.edu>
2022-08-30 15:50:25 -0400

    Basic table and game add func

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -1,3 +1,66 @@
-// Add some Javascript code here, to run on the front end.
+const fetchGames = async () =>{
+    const gamesRequest = await fetch("/games");
+    const gamesData = await gamesRequest.json();
+
+    return gamesData
+}
+
+const updateTable = async () => {
+    const gamesData = await fetchGames();
+    gamesData.forEach(game => {
+        addGameToTable(game)
+    });
+}
+
+const addGameToTable = (gameObj) => {
+    const table = document.querySelector('tbody');
+    table.innerHTML+=generateRow(gameObj);
+}
+
+window.onload = async function () {
+    await updateTable();
+
+    const button = document.querySelector('#btnSubmit')
+    button.addEventListener("click", async (evt) => {
+        evt.preventDefault()
+
+        const dateValue = document.querySelector("#datePicker").value
+        const hitsValue = document.querySelector("#hitsField").value
+        const atBatsValue = document.querySelector("#atBatsField").value
+
+        console.log(dateValue, hitsValue, atBatsValue)
+
+        const dataPostResponse = await fetch("/games", {
+            method:"POST",
+            body: JSON.stringify({
+                dateValue,
+                hitsValue,
+                atBatsValue
+            })
+        })
+
+        if(dataPostResponse.status === 200){
+            addGameToTable(await dataPostResponse.json())
+        }else{
+            console.error("AHHHHHHHHH")
+        }
+
+    })
+}
+/**
+ * Helper function that generates the html for a single row in the table given a single game's data
+ * @param {Object} statsData data object of a single game
+ * 
+ * @returns html for a single game to be added to the table
+ */
+const generateRow = (statsData) => {
+    return `<tr>
+        <td>${statsData.id}</td>
+        <td>${statsData.date}</td>
+        <td>${statsData.hits}</td>
+        <td>${statsData.atBats}</td>
+        <td>${statsData.avg.toFixed(3)}</td>
+    </tr>`
+}
 
 console.log("Welcome to assignment 2!")
--- server.improved.js
+++ server.improved.js
@@ -1,72 +1,86 @@
-const http = require( 'http' ),
-      fs   = require( 'fs' ),
-      // IMPORTANT: you must run `npm install` in the directory for this assignment
-      // to install the mime library used in the following line of code
-      mime = require( 'mime' ),
-      dir  = 'public/',
-      port = 3000
+const http = require('http'),
+  fs = require('fs'),
+  mime = require('mime'),
+  dir = 'public/',
+  port = 3000
 
-const appdata = [
-  { 'model': 'toyota', 'year': 1999, 'mpg': 23 },
-  { 'model': 'honda', 'year': 2004, 'mpg': 30 },
-  { 'model': 'ford', 'year': 1987, 'mpg': 14} 
+
+
+const statsData = [
+  { "id": 0, "date": "2022-08-27", "hits": 2, "atBats": 3, "avg": 2 / 3 },
+  { "id": 1, "date": "2022-08-28", "hits": 3, "atBats": 3, "avg": 1 },
+  { "id": 2, "date": "2022-08-29", "hits": 1, "atBats": 3, "avg": 1 / 3 }
 ]
 
-const server = http.createServer( function( request,response ) {
-  if( request.method === 'GET' ) {
-    handleGet( request, response )    
-  }else if( request.method === 'POST' ){
-    handlePost( request, response ) 
+const server = http.createServer(function (request, response) {
+  if (request.method === 'GET') {
+    handleGet(request, response)
+  } else if (request.method === 'POST') {
+    handlePost(request, response)
   }
 })
 
-const handleGet = function( request, response ) {
-  const filename = dir + request.url.slice( 1 ) 
+const handleGet = function (request, response) {
+  const filename = dir + request.url.slice(1)
 
-  if( request.url === '/' ) {
-    sendFile( response, 'public/index.html' )
-  }else{
-    sendFile( response, filename )
+  if (request.url === '/') {
+    sendFile(response, 'public/index.html')
+  } else if (request.url === "/games") {
+    response.writeHeader(200, { 'Content-Type': "applcation/json" })
+    response.end(JSON.stringify(statsData))
+  } else {
+    sendFile(response, filename)
   }
 }
 
-const handlePost = function( request, response ) {
+const handlePost = function (request, response) {
   let dataString = ''
 
-  request.on( 'data', function( data ) {
-      dataString += data 
-  })
+  if (request.url === "/games") {
 
-  request.on( 'end', function() {
-    console.log( JSON.parse( dataString ) )
+    request.on('data', function (data) {
+      dataString += data
+    })
 
-    // ... do something with the data here!!!
+    request.on('end', function () {
+      const requestData = JSON.parse(dataString);
+      const requestGameObject = {
+        id: statsData.length,
+        date: requestData.dateValue,
+        hits: requestData.hitsValue,
+        atBats: requestData.atBatsValue,
+        avg: requestData.hitsValue/requestData.atBatsValue
+      }
+      
 
-    response.writeHead( 200, "OK", {'Content-Type': 'text/plain' })
-    response.end()
+      statsData.push(requestGameObject)
+
+      response.writeHead(200, "OK", { 'Content-Type': 'text/plain' })
+      response.end(JSON.stringify(requestGameObject))
+    })
+  }
+}
+
+const sendFile = function (response, filename) {
+  const type = mime.getType(filename)
+
+  fs.readFile(filename, function (err, content) {
+
+    // if the error = null, then we've loaded the file successfully
+    if (err === null) {
+
+      // status code: https://httpstatuses.com
+      response.writeHeader(200, { 'Content-Type': type })
+      response.end(content)
+
+    } else {
+
+      // file not found, error code 404
+      response.writeHeader(404)
+      response.end('404 Error: File Not Found')
+
+    }
   })
 }
 
-const sendFile = function( response, filename ) {
-   const type = mime.getType( filename ) 
-
-   fs.readFile( filename, function( err, content ) {
-
-     // if the error = null, then we've loaded the file successfully
-     if( err === null ) {
-
-       // status code: https://httpstatuses.com
-       response.writeHeader( 200, { 'Content-Type': type })
-       response.end( content )
-
-     }else{
-
-       // file not found, error code 404
-       response.writeHeader( 404 )
-       response.end( '404 Error: File Not Found' )
-
-     }
-   })
-}
-
-server.listen( process.env.PORT || port )
+server.listen(process.env.PORT || port)
