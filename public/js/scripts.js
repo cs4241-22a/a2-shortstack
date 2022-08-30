1f635205f81d0280b9c2ad8b6c99afa2c2165ad7
dtavana <datavana@gmail.com>
2022-08-30 17:22:56 -0400

    Add base functions to scripts.js

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -1,3 +1,43 @@
-// Add some Javascript code here, to run on the front end.
+const submit = function (e) {
+  // prevent default form action from being carried out
+  e.preventDefault();
 
-console.log("Welcome to assignment 2!")
+  const playerNameInput = document.querySelector("#playerName");
+  const playerScoreInput = document.querySelector("#playerScore");
+  const winResultInput = document.querySelector("#winResult");
+
+  const rowData = {
+      playerName: playerNameInput.value,
+      playerScore: parseInt(playerScoreInput.value),
+      winResult: winResultInput.checked,
+    },
+    body = JSON.stringify({ rowData });
+
+  fetch("/", {
+    method: "POST",
+    body,
+  })
+    .then((response) => response.json())
+    .then((data) => updateTableWithData(data));
+
+  return false;
+};
+
+const updateTableWithData = function (currentData) {
+  // Taken from https://stackoverflow.com/a/16270116
+  $("#resultsTable:not(:first)").remove();
+  const table = document.querySelector("#resultsTable");
+  console.log(currentData);
+  currentData.forEach((element, index) => {
+    const row = table.insertRow(index);
+    for (const value of Object.values(element)) {
+      const cell = row.insertCell();
+      cell.innerHTML = value;
+    }
+  });
+};
+
+window.onload = function () {
+  const button = document.querySelector("#submit");
+  button.onclick = submit;
+};
