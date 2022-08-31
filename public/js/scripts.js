4dfc393df2edeadc809b09a35b102287305a2d9e
dtavana <datavana@gmail.com>
2022-08-31 11:21:50 -0400

    Add onclick functions to modify and delete cells

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -1,17 +1,21 @@
-const submit = function (e) {
-  // prevent default form action from being carried out
-  e.preventDefault();
-
+const getRowData = function () {
   const playerNameInput = document.querySelector("#playerName");
   const playerScoreInput = document.querySelector("#playerScore");
   const winResultInput = document.querySelector("#winResult");
 
-  const rowData = {
-      playerName: playerNameInput.value,
-      playerScore: parseInt(playerScoreInput.value),
-      winResult: winResultInput.checked,
-    },
-    body = JSON.stringify({ rowData });
+  return {
+    playerName: playerNameInput.value,
+    playerScore: parseInt(playerScoreInput.value),
+    winResult: winResultInput.checked,
+  };
+};
+
+const addEntry = function (e) {
+  // prevent default form action from being carried out
+  e.preventDefault();
+
+  const rowData = getRowData();
+  const body = JSON.stringify({ rowData });
 
   fetch("/", {
     method: "POST",
@@ -26,19 +30,58 @@ const submit = function (e) {
   return false;
 };
 
+const deleteEntry = function (rowIndex) {
+  const body = JSON.stringify({ rowIndex });
+
+  fetch("/", {
+    method: "DELETE",
+    body,
+  })
+    .then((response) => response.json())
+    .then((data) => {
+      $("#resultsTable").find("tr:gt(0)").remove();
+      updateTableWithData(data);
+    });
+
+  return false;
+};
+
+const modifyEntry = function (rowIndex) {
+  const rowData = getRowData();
+  const body = JSON.stringify({ rowData, rowIndex });
+
+  fetch("/", {
+    method: "PUT",
+    body,
+  })
+    .then((response) => response.json())
+    .then((data) => {
+      $("#resultsTable").find("tr:gt(0)").remove();
+      updateTableWithData(data);
+    });
+
+  return false;
+};
+
 const updateTableWithData = function (currentData) {
   const table = document.querySelector("#resultsTable");
   console.log(currentData);
-  currentData.forEach((element) => {
+  currentData.forEach((element, index) => {
     const row = table.insertRow();
     for (const value of Object.values(element)) {
       const cell = row.insertCell();
       cell.innerHTML = value;
     }
+    const modifyCell = row.insertCell();
+    const deleteCell = row.insertCell();
+    modifyCell.setAttribute("class", "modifyCell");
+    deleteCell.setAttribute("class", "deleteCell");
+    modifyCell.setAttribute("onclick", `modifyEntry(${index})`);
+    deleteCell.setAttribute("onclick", `deleteEntry(${index})`);
   });
 };
 
 window.onload = function () {
   const button = document.querySelector("#submit");
-  button.onclick = submit;
+  button.onclick = addEntry;
 };
