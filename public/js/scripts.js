199f8c1da0ac2053c1c01d6bc3170d148d75e270
Caleb Talley <86822049+CalebTalley2023@users.noreply.github.com>
2022-09-04 17:30:34 -0400

    Made Basic List
    click  the "+" to add to list
    
    click the "to do" item to cross it out
    
    double click the "to do" item to delete it

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -1,3 +1,29 @@
 // Add some Javascript code here, to run on the front end.
-
 console.log("Welcome to assignment 2!")
+
+// getElementbyID: gets element from our HTML based on it's ID
+let addToDoButton = document.getElementById("taskNewButton");
+let toDoList = document.getElementById("toDoList");
+let newTask = document.getElementById("newTask");
+
+//addEventListener: waits for an event to occur 
+//when a click occurs, this function occurs
+addToDoButton.addEventListener('click', function(){
+    
+    var paragraph = document.createElement('p');
+    // put our newTask value into our newly made paragraph
+    paragraph.innerText = newTask.value;
+    // add this paragraph to our toDoList 
+    toDoList.appendChild(paragraph);
+    
+
+    // this clears our newTask input field so that we can add a new task
+    newTask.value = "";
+    paragraph.addEventListener('click',function(){
+        paragraph.style.textDecoration = "line-through";
+    })
+    paragraph.addEventListener('dblclick',function(){
+        toDoList.removeChild(paragraph);
+    })
+})
+
