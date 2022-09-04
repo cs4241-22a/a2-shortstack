07c213dd5db94ccd7757a1dd2f493fb3086340be
npollock14 <ndpollock@wpi.edu>
2022-09-04 04:05:00 -0400

    idea and basic styling

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -1,3 +1,48 @@
-// Add some Javascript code here, to run on the front end.
+const submitbuy = function (e) {
+  // prevent default form action from being carried out
+  e.preventDefault();
 
-console.log("Welcome to assignment 2!")
+  console.log("buy button clicked");
+
+  const input = document.querySelector("#stockinput"),
+    json = { stockinput: input.value },
+    body = JSON.stringify(json);
+
+  fetch("/submit", {
+    method: "POST",
+    body,
+  }).then(function (response) {
+    // do something with the reponse
+    console.log(response);
+  });
+
+  return false;
+};
+
+const submitsell = function (e) {
+  // prevent default form action from being carried out
+  e.preventDefault();
+
+  console.log("sell button clicked");
+
+  const input = document.querySelector("#stockinput"),
+    json = { stockinput: input.value },
+    body = JSON.stringify(json);
+
+  fetch("/submit", {
+    method: "POST",
+    body,
+  }).then(function (response) {
+    // do something with the reponse
+    console.log(response);
+  });
+
+  return false;
+};
+
+window.onload = function () {
+  const buybutton = document.querySelector("#buybutton");
+  buybutton.onclick = submitbuy;
+  const sellbutton = document.querySelector("#sellbutton");
+  sellbutton.onclick = submitsell;
+};
--- server.improved.js
+++ server.improved.js
@@ -1,72 +1,90 @@
-const http = require( 'http' ),
-      fs   = require( 'fs' ),
-      // IMPORTANT: you must run `npm install` in the directory for this assignment
-      // to install the mime library used in the following line of code
-      mime = require( 'mime' ),
-      dir  = 'public/',
-      port = 3000
+const http = require("http"),
+  fs = require("fs"),
+  // IMPORTANT: you must run `npm install` in the directory for this assignment
+  // to install the mime library used in the following line of code
+  mime = require("mime"),
+  bent = require("bent"),
+  dir = "public/",
+  port = 3000;
+
+const getJSON = bent("json");
 
 const appdata = [
-  { 'model': 'toyota', 'year': 1999, 'mpg': 23 },
-  { 'model': 'honda', 'year': 2004, 'mpg': 30 },
-  { 'model': 'ford', 'year': 1987, 'mpg': 14} 
-]
+  { model: "toyota", year: 1999, mpg: 23 },
+  { model: "honda", year: 2004, mpg: 30 },
+  { model: "ford", year: 1987, mpg: 14 },
+];
 
-const server = http.createServer( function( request,response ) {
-  if( request.method === 'GET' ) {
-    handleGet( request, response )    
-  }else if( request.method === 'POST' ){
-    handlePost( request, response ) 
+const server = http.createServer(function (request, response) {
+  if (request.method === "GET") {
+    handleGet(request, response);
+  } else if (request.method === "POST") {
+    handlePost(request, response);
   }
-})
+});
 
-const handleGet = function( request, response ) {
-  const filename = dir + request.url.slice( 1 ) 
+const handleGet = function (request, response) {
+  const filename = dir + request.url.slice(1);
 
-  if( request.url === '/' ) {
-    sendFile( response, 'public/index.html' )
-  }else{
-    sendFile( response, filename )
+  if (request.url === "/") {
+    sendFile(response, "public/index.html");
+  } else {
+    sendFile(response, filename);
   }
-}
+};
 
-const handlePost = function( request, response ) {
-  let dataString = ''
+const handlePost = function (request, response) {
+  let dataString = "";
 
-  request.on( 'data', function( data ) {
-      dataString += data 
-  })
+  request.on("data", function (data) {
+    dataString += data;
+  });
 
-  request.on( 'end', function() {
-    console.log( JSON.parse( dataString ) )
+  request.on("end", function () {
+    let dataObject = JSON.parse(dataString);
+    console.log(dataObject);
+
+    const url =
+      "https://query1.finance.yahoo.com/v8/finance/chart/" +
+      dataObject["stockinput"] +
+      "?region=US&lang=en-US&includePrePost=false&interval=2m&useYfid=true&range=1d&corsDomain=finance.yahoo.com&.tsrc=finance";
+
+    getJSON(url)
+      .then((res) => {
+        //log the data
+        console.log(res.chart.result[0].meta.regularMarketPrice);
+        //send the data back to the client
+        response.writeHeader(200, { "Content-Type": "application/json" });
+        response.end(
+          JSON.stringify(res.chart.result[0].meta.regularMarketPrice)
+        );
+      }) //catch any errors
+      .catch((err) => {
+        console.log(err);
+      });
 
     // ... do something with the data here!!!
 
-    response.writeHead( 200, "OK", {'Content-Type': 'text/plain' })
-    response.end()
-  })
-}
+    response.writeHead(200, "OK", { "Content-Type": "text/plain" });
+    response.end("post received");
+  });
+};
 
-const sendFile = function( response, filename ) {
-   const type = mime.getType( filename ) 
+const sendFile = function (response, filename) {
+  const type = mime.getType(filename);
 
-   fs.readFile( filename, function( err, content ) {
+  fs.readFile(filename, function (err, content) {
+    // if the error = null, then we've loaded the file successfully
+    if (err === null) {
+      // status code: https://httpstatuses.com
+      response.writeHeader(200, { "Content-Type": type });
+      response.end(content);
+    } else {
+      // file not found, error code 404
+      response.writeHeader(404);
+      response.end("404 Error: File Not Found");
+    }
+  });
+};
 
-     // if the error = null, then we've loaded the file successfully
-     if( err === null ) {
-
-       // status code: https://httpstatuses.com
-       response.writeHeader( 200, { 'Content-Type': type })
-       response.end( content )
-
-     }else{
-
-       // file not found, error code 404
-       response.writeHeader( 404 )
-       response.end( '404 Error: File Not Found' )
-
-     }
-   })
-}
-
-server.listen( process.env.PORT || port )
+server.listen(process.env.PORT || port);
