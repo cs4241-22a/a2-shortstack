3773920a25dd24c12030a11414f8a2483ceffedf
Gabe Camacho <gabestuf@gmail.com>
2022-09-05 12:10:30 -0400

    hw2

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -1,3 +1,65 @@
-// Add some Javascript code here, to run on the front end.
+const submit = function (e) {
+    // prevent default form action from being carried out
+    e.preventDefault()
 
-console.log("Welcome to assignment 2!")
+    inputTitle = document.querySelector('#title')
+    inputDesc = document.querySelector('#desc')
+    inputLoc = document.querySelector('#loc')
+
+    const json = {
+        title: inputTitle.value,
+        description: inputDesc.value,
+        location: inputLoc.value
+    },
+        body = JSON.stringify(json)
+
+    fetch('/submit', {
+        method: 'POST',
+        body
+    })
+        .then(async (response) => {
+            // do something with the reponse 
+            let newData = await response.json()
+            generateTable(newData)
+        })
+
+    return false
+}
+
+const getData = async () => {
+    body = JSON.stringify([])
+    fetch('/getData')
+        .then(async (response) => {
+            let newData = await response.json()
+            generateTable(newData)
+        })
+}
+
+const generateTable = (dataJSON) => {
+
+
+
+    const tableElement = document.getElementById("dataTable")
+    tableElement.innerHTML = ""
+    const tableHeadTR = document.createElement("tr")
+    tableHeadTR.setAttribute("id", "tableheader")
+
+    tableHeadTR.innerHTML = '<th>Title</th><th>Description</th><th>Location</th> '
+    tableElement.appendChild(tableHeadTR)
+
+    dataJSON.map((val) => {
+        const tableRowElement = document.createElement("tr")
+        tableRowElement.innerHTML = ` <th>${val.title}</th>
+                                    <th>${val.description}</th>
+                                    <th>${val.location}<th>`
+        tableElement.appendChild(tableRowElement)
+    })
+
+    document.body.appendChild(tableElement)
+}
+
+window.onload = function () {
+    const button = document.querySelector('button')
+    button.onclick = submit
+    getData()
+}
