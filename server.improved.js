37e8b69526202536e6e0a72a36e0bb99dcd8acc3
unknown <quinguyen.dev@gmail.com>
2022-09-05 01:09:29 -0400

    [NEW] Added server functionality for passing in data.

--- server.improved.js
+++ server.improved.js
@@ -1,72 +1,114 @@
-const http = require( 'http' ),
-      fs   = require( 'fs' ),
-      // IMPORTANT: you must run `npm install` in the directory for this assignment
-      // to install the mime library used in the following line of code
-      mime = require( 'mime' ),
-      dir  = 'public/',
-      port = 3000
+const { Console } = require("console");
+
+const http = require("http"),
+  fs = require("fs"),
+  mime = require("mime"),
+  dir = "public/",
+  port = 3000;
 
 const appdata = [
-  { 'model': 'toyota', 'year': 1999, 'mpg': 23 },
-  { 'model': 'honda', 'year': 2004, 'mpg': 30 },
-  { 'model': 'ford', 'year': 1987, 'mpg': 14} 
-]
+  {
+    priority: "high",
+    type: "mqp",
+    title: "Initial Sponsor Meeting",
+    description: "[example text0]",
+    deadline: "2022-09-09 23:59",
+    creation_time: "2022-9-2 11:01",
+  },
+  {
+    priority: "urgent",
+    type: "academic",
+    title: "CS 4241 Assignment 2",
+    description: "[example text1]",
+    deadline: "2022-09-08 11:59",
+    creation_time: "2022-9-2 13:10",
+  },
+  {
+    priority: "low",
+    type: "other",
+    title: "Gym [LEG DAY]",
+    description: "[example text2]",
+    deadline: "2022-09-09 23:59",
+    creation_time: "2022-9-5 18:50",
+  },
+];
 
-const server = http.createServer( function( request,response ) {
-  if( request.method === 'GET' ) {
-    handleGet( request, response )    
-  }else if( request.method === 'POST' ){
-    handlePost( request, response ) 
+/**
+ * Creates an instance of Server
+ */
+const server = http.createServer(function (request, response) {
+  if (request.method === "GET") {
+    handleGet(request, response);
+  } else if (request.method === "POST") {
+    handlePost(request, response);
   }
-})
+});
 
-const handleGet = function( request, response ) {
-  const filename = dir + request.url.slice( 1 ) 
+/**
+ *
+ * @param {*} request
+ * @param {*} response
+ */
+const handleGet = function (request, response) {
+  const filename = dir + request.url.slice(1);
 
-  if( request.url === '/' ) {
-    sendFile( response, 'public/index.html' )
-  }else{
-    sendFile( response, filename )
+  /* Request routing */
+  if (request.url === "/") {
+    sendFile(response, "public/index.html");
+  } else {
+    sendFile(response, filename);
   }
-}
+};
 
-const handlePost = function( request, response ) {
-  let dataString = ''
+const handlePost = function (request, response) {
+  let dataString = "";
 
-  request.on( 'data', function( data ) {
-      dataString += data 
-  })
+  request.on("data", function (data) {
+    dataString += data;
+  });
 
-  request.on( 'end', function() {
-    console.log( JSON.parse( dataString ) )
+  request.on("end", function () {
+    const data = JSON.parse(dataString);
 
-    // ... do something with the data here!!!
+    /* Get current date and time */
+    const date = new Date();
+    const present = `${date.getFullYear()}-${
+      date.getMonth() + 1
+    }-${date.getDate()} ${date.getHours()}:${date.getMinutes()}`;
 
-    response.writeHead( 200, "OK", {'Content-Type': 'text/plain' })
-    response.end()
-  })
-}
+    data.creation_time = present;
 
-const sendFile = function( response, filename ) {
-   const type = mime.getType( filename ) 
+    /* Check if there is a given deadline */
+    if (data.deadline) {
+      data.deadline = data.deadline.replace("T", " ");
+      console.log(data.deadline);
+    } else {
+      // set a new deadline for the stuff
+    }
 
-   fs.readFile( filename, function( err, content ) {
+    appdata.push(data);
 
-     // if the error = null, then we've loaded the file successfully
-     if( err === null ) {
+    response.writeHead(200, "OK", { "Content-Type": "text/plain" });
+    response.end(JSON.stringify(appdata));
+  });
+};
 
-       // status code: https://httpstatuses.com
-       response.writeHeader( 200, { 'Content-Type': type })
-       response.end( content )
+const sendFile = function (response, filename) {
+  /* String parsing to determine mime type */
+  const type = mime.getType(filename);
 
-     }else{
+  fs.readFile(filename, function (err, content) {
+    /* If the error is null, then file has loaded successfully. */
+    if (err === null) {
+      // status code: https://httpstatuses.com
+      response.writeHeader(200, { "Content-Type": type });
+      response.end(content);
+    } else {
+      // file not found, error code 404
+      response.writeHeader(404);
+      response.end("404 Error: File Not Found");
+    }
+  });
+};
 
-       // file not found, error code 404
-       response.writeHeader( 404 )
-       response.end( '404 Error: File Not Found' )
-
-     }
-   })
-}
-
-server.listen( process.env.PORT || port )
+server.listen(process.env.PORT || port);
