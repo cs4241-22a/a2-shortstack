d9dcc69ba7fbb852aed0eb2cb60359b765affc1b
Ben Sakac <101744805+benSakac-WPI@users.noreply.github.com>
2022-09-05 17:14:28 -0400

    D
    Delete functionality added

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -1,85 +1,121 @@
 const submit = function (e) {
-    // prevent default form action from being carried out
-    e.preventDefault();
-    
-    let inputIDs = ["#firstName", "#lastName", "#birthday", "#giftIdea"]; //Element IDs for each input field
-    let formLabels = [];
-    let formInputs = [];
+	// prevent default form action from being carried out
+	e.preventDefault();
 
-    for (let index = 0; index < inputIDs.length; index++) {
-        let ID = inputIDs[index];
-        let input = document.querySelector(ID);
-        let fieldName = ID.slice(1); //JSON object key
-        formLabels.push(fieldName);
-        formInputs.push(input.value);
-    }
-    function convertToObj(a, b) {
-        if (a.length != b.length || a.lenght || b.length == 0){
-            return null;
-        }
-    let obj = {};	
-    json = a.forEach((k, i) => {obj[k] = b[i]});
-    return obj;
-    }
-    json = convertToObj(formLabels, formInputs);
-    body = JSON.stringify( json );
-    fetch("/submit", {
-        method: "POST",
-        body,
-    })
-    .then( response => response.json() )
-    .then( json => console.log( json ) )
+	let inputIDs = ["#firstName", "#lastName", "#birthday", "#giftIdea"]; //Element IDs for each input field
+	let formLabels = [];
+	let formInputs = [];
 
-    renderTable();
+	for (let index = 0; index < inputIDs.length; index++) {
+		let ID = inputIDs[index];
+		let input = document.querySelector(ID);
+		let fieldName = ID.slice(1); //JSON object key
+		formLabels.push(fieldName);
+		formInputs.push(input.value);
+	}
+	function convertToObj(a, b) {
+		if (a.length != b.length || a.length == 0 || b.length == 0) {
+			return null;
+		}
+		let obj = {};
+		json = a.forEach((k, i) => {
+			obj[k] = b[i];
+		});
+		return obj;
+	}
 
-    
-    return false;
+	json = convertToObj(formLabels, formInputs);
+	body = JSON.stringify(json);
+
+	fetch("/submit", {
+		method: "POST",
+		body,
+	})
+		.then((response) => response.json())
+		.then((json) => console.log(json));
+
+	renderTable();
+
+	return false;
 };
 
 window.onload = function () {
-    const button = document.querySelector("button");
-    button.onclick = submit;
-    renderTable();
+	const button = document.querySelector("button");
+	button.onclick = submit;
+	renderTable();
 };
 
-
 function renderTable() {
-    fetch("/birthdays", {
-        method: "GET",
-    })
-    .then (response => response.json())
-    .then (appdata => {
-        console.log(JSON.stringify(appdata))
-        console.log(typeof(JSON.stringify(appdata)))
-        if (JSON.stringify(appdata) === "[]") {
-            console.log("No data to display :(")
-        } else {
-            let resultsTable = document.querySelector("table.results");
-            resultsTable.innerHTML = " "
-            let hRow = document.createElement('tr');
-            hRow.className = 'results';
-            resultsTable.appendChild(hRow);
-            hRow.innerHTML = '<th>First Name</th><th>Last Name</th><th>Birthday</th><th>Days Until Next Birthday</th><th>Gift Idea</th><th style="background-color: black;"></th>';
-            
-            for (let entries in appdata) {
-                let row = document.createElement('tr');
-                row.className = 'results';
-                resultsTable.appendChild(row);
-                let cell1 = row.insertCell(0);
-                let cell2 = row.insertCell(1);
-                let cell3 = row.insertCell(2);
-                let cell4 = row.insertCell(3);
-                let cell5 = row.insertCell(4);
-                let cell6 = row.insertCell(5);
-                let btn = cell6.appendChild(document.createElement('button'))
-                
-                cell1.innerHTML = `${appdata[entries].firstName}`;
-                cell2.innerHTML = `${appdata[entries].lastName}`;
-                cell3.innerHTML = `${appdata[entries].birthday}`;
-                cell4.innerHTML = `${appdata[entries].daysUntil}`;
-                cell5.innerHTML = `${appdata[entries].giftIdea}`;
-                btn.innerHTML = "Delete";
-                }
-            }
-    })
+	fetch("/birthdays", {
+		method: "GET",
+	})
+		.then((response) => response.json())
+		.then((appdata) => {
+			console.log(JSON.stringify(appdata));
+			//Do not render table if no birthdays have been added to the server yet, otherwise render the results table content
+			if (JSON.stringify(appdata) === "[]") {
+				console.log("No data to display :(");
+				let resultsTable = document.querySelector("table.results");
+				resultsTable.innerHTML = " ";
+				let message = document.createElement("p");
+				message.innerHTML =
+					"There are no saved birthdays. Add a new birthday using the form above!";
+				document.querySelector("main").appendChild(message);
+			} else {
+				//Remove no birthdays message if it exists
+				if (document.querySelector("p") != null) {
+					let message = document.querySelector("p");
+					document.querySelector("main").removeChild(message);
+				}
+				let resultsTable = document.querySelector("table.results");
+				resultsTable.innerHTML = " ";
+				let hRow = document.createElement("tr");
+				hRow.className = "results";
+				resultsTable.appendChild(hRow);
+				hRow.innerHTML =
+					`<th>First Name</th>
+                    <th>Last Name</th>
+                    <th>Birthday</th>
+                    <th>Days Until Next Birthday</th>
+                    <th>Gift Idea</th>
+                    <th style="background-color: black;"></th>`;
+
+				for (let entries in appdata) {
+					let row = document.createElement("tr");
+					row.className = "results";
+					resultsTable.appendChild(row);
+					let cell1 = row.insertCell(0);
+					let cell2 = row.insertCell(1);
+					let cell3 = row.insertCell(2);
+					let cell4 = row.insertCell(3);
+					let cell5 = row.insertCell(4);
+					let cell6 = row.insertCell(5);
+					let btn = cell6.appendChild(document.createElement("button"));
+
+					cell1.innerHTML = `${appdata[entries].firstName}`;
+					cell2.innerHTML = `${appdata[entries].lastName}`;
+					cell3.innerHTML = `${appdata[entries].birthday}`;
+					cell4.innerHTML = `${appdata[entries].daysUntil}`;
+					cell5.innerHTML = `${appdata[entries].giftIdea}`;
+					btn.className = `${appdata[entries].submitTime}`;
+					btn.innerHTML = "Delete";
+					btn.onclick = function () {
+						remove(btn.className);
+					};
+				}
+			}
+		});
+}
+
+function remove(UID) {
+	console.log(UID);
+	let body = UID;
+	fetch("/remove", {
+		method: "POST",
+		body,
+	})
+		.then((response) => response.json())
+		.then((json) => console.log(json));
+
+	renderTable();
 }
--- server.improved.js
+++ server.improved.js
@@ -1,99 +1,97 @@
-const http = require( 'http' ),
-      fs   = require( 'fs' ),
-      // IMPORTANT: you must run `npm install` in the directory for this assignment
-      // to install the mime library used in the following line of code
-      mime = require( 'mime' ),
-      dir  = 'public/',
-      port = 3000
+const http = require("http"),
+	fs = require("fs"),
+	// IMPORTANT: you must run `npm install` in the directory for this assignment
+	// to install the mime library used in the following line of code
+	mime = require("mime"),
+	dir = "public/",
+	port = 3000;
 
-const appdata = []
+const appdata = [];
+
+const server = http.createServer(function (request, response) {
+	if (request.method === "GET") {
+		handleGet(request, response);
+	} else if (request.method === "POST") {
+		handlePost(request, response);
+	}
+});
+
+const handleGet = function (request, response) {
+	const filename = dir + request.url.slice(1);
+
+	if (request.url === "/") {
+		sendFile(response, "public/index.html");
+	} else if (request.url === "/birthdays") {
+		response.writeHeader(200, { "Content-Type": "text/plain" });
+		response.end(JSON.stringify(appdata));
+	} else {
+		sendFile(response, filename);
+	}
+};
+
+const handlePost = function (request, response) {
+	let dataString = "";
+
+	request.on("data", function (data) {
+		dataString += data;
+	});
+	console.log(dataString);
+	request.on("end", function () {
+		if (request.url === "/submit") {
+			let submission = JSON.parse(dataString);
+			//Calculated field
+			let calcField = {
+				daysUntil: `${daysUntilCalc(submission.birthday)}`,
+				submitTime: `${Date.now()}`,
+			};
+			let newEntry = Object.assign(submission, calcField);
+			appdata.push(newEntry);
+			response.writeHead(200, "OK", { "Content-Type": "text/plain" });
+			response.end(JSON.stringify(appdata));
+		} else if (request.url === "/remove") {
+			let UID = dataString;
+			let index = appdata.findIndex((element) => {
+				if (element.submitTime === UID) {
+					return true;
+				}
+			});
+			appdata.splice(index, 1);
+			response.writeHead(200, "OK", { "Content-Type": "text/plain" });
+			response.end(JSON.stringify(appdata));
+		}
+	});
+};
+
+const sendFile = function (response, filename) {
+	const type = mime.getType(filename);
+
+	fs.readFile(filename, function (err, content) {
+		// if the error = null, then we've loaded the file successfully
+		if (err === null) {
+			// status code: https://httpstatuses.com
+			response.writeHeader(200, { "Content-Type": type });
+			response.end(content);
+		} else {
+			// file not found, error code 404
+			response.writeHeader(404);
+			response.end("404 Error: File Not Found");
+		}
+	});
+};
+
+server.listen(process.env.PORT || port);
 
 //Function to calculate the number of days until next birthday
 function daysUntilCalc(string) {
-  let currentDay = new Date();
-  let birthArray = string.split("-");
-  let birthday = new Date(birthArray[0], birthArray[1]-1, birthArray[2]); //Month is subtracted by 1 since JS counts months 0-11
-  //Set current year or the next year if you already had birthday this year
-  birthday.setFullYear(currentDay.getFullYear());
-  if (currentDay > birthday) {
-  birthday.setFullYear(currentDay.getFullYear() + 1);
-  }
-  //Calculate difference between days
-  let daysUntil = Math.floor((birthday - currentDay) / (1000*60*60*24))
-  return daysUntil
-};
-
-const server = http.createServer( function( request,response ) {
-  if( request.method === 'GET' ) {
-    handleGet( request, response )    
-  }else if( request.method === 'POST' ){
-    handlePost( request, response ) 
-  }
-})
-
-const handleGet = function( request, response ) {
-  const filename = dir + request.url.slice( 1 ) 
-
-  if( request.url === '/' ) {
-    sendFile( response, 'public/index.html' )
-  
-  }
-  else if (request.url === '/birthdays') {
-    response.writeHeader(200, {'Content-Type': 'text/plain'})
-    response.end(JSON.stringify(appdata))
-  }
-  else {
-    sendFile( response, filename )
-  }
+	let currentDay = new Date();
+	let birthArray = string.split("-");
+	let birthday = new Date(birthArray[0], birthArray[1] - 1, birthArray[2]); //Month is subtracted by 1 since JS counts months 0-11
+	//Set current year or the next year if you already had birthday this year
+	birthday.setFullYear(currentDay.getFullYear());
+	if (currentDay > birthday) {
+		birthday.setFullYear(currentDay.getFullYear() + 1);
+	}
+	//Calculate difference between days
+	let daysUntil = Math.floor((birthday - currentDay) / (1000 * 60 * 60 * 24));
+	return daysUntil;
 }
-
-const handlePost = function( request, response ) {
-  let dataString = ''
-
-  request.on( 'data', function( data ) {
-      dataString += data 
-  })
-
-  request.on( 'end', function() {
-    console.log( JSON.parse( dataString ) );
-
-    //Convert data string back to object to add calculated field
-    var submission = JSON.parse(dataString);
-
-    //Calculated field 
-    var calcField = {
-      daysUntil: `${daysUntilCalc(submission.birthday)}`
-    }
-
-    var newEntry = Object.assign(submission, calcField);
-
-    appdata.push(newEntry);
-
-    response.writeHead( 200, "OK", {'Content-Type': 'text/plain' });
-    response.end(JSON.stringify(appdata));
-  })
-}
-
-const sendFile = function( response, filename ) {
-   const type = mime.getType( filename ) 
-
-   fs.readFile( filename, function( err, content ) {
-
-     // if the error = null, then we've loaded the file successfully
-     if( err === null ) {
-
-       // status code: https://httpstatuses.com
-       response.writeHeader( 200, { 'Content-Type': type })
-       response.end( content )
-
-     }else{
-
-       // file not found, error code 404
-       response.writeHeader( 404 )
-       response.end( '404 Error: File Not Found' )
-
-     }
-   })
-}
-
-server.listen( process.env.PORT || port )
