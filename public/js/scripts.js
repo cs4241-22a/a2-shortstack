9bb2c9cafe38dce834167919262ccb431bf9dc52
seansta18 <sdmcmillan@wpi.edu>
2022-09-06 16:57:48 -0400

    Added CSS Styling and Started to work on JS files

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -1,3 +1,25 @@
-// Add some Javascript code here, to run on the front end.
+const submit = function( e ) {
+    // prevent default form action from being carried out
+    e.preventDefault()
 
-console.log("Welcome to assignment 2!")
+    const input = document.querySelector( '#yourname' ),
+          json = { yourname: input.value },
+          body = JSON.stringify( json )
+
+    fetch( './server.js', {
+      method:'POST',
+      body 
+    })
+    .then( function( response ) {
+      document.body.style.backgroundColor = "red";
+
+      console.log( response )
+    })
+
+    return false
+  }
+
+  window.onload = function() {
+    const button = document.querySelector( 'button' )
+    button.onclick = submit
+  }
