cdf057c69ce44bc21bbc450533bdaf4fb5a9fe1f
Jacquelyn Lopez <jlopez2@wpi.edu>
2022-09-06 22:55:21 -0400

    project complete!

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -1,3 +1,139 @@
-// Add some Javascript code here, to run on the front end.
-
 console.log("Welcome to assignment 2!")
+
+let editFlag = -1
+
+const submit = function( e ) {
+    e.preventDefault()
+
+    const input1 = document.querySelector( '#studentID' ),
+          input2 = document.querySelector( '#name' ),
+          input3 = document.querySelector( '#appointment' ),
+          json = { studentID: input1.value, name: input2.value, appointment: input3.value },
+          body = editFlag != -1 ? "EDIT" + editFlag + JSON.stringify( json ) : JSON.stringify( json )
+
+
+    editFlag = -1
+
+    fetch( '/submit', {
+      method:'POST',
+      body 
+    })
+    .then( response => response.json() )
+    .then( function ( json ) {
+      // do something with the reponse
+      const element = document.querySelector('#taskTable')
+      
+      for(let i = 1; i <= json.length; i++){
+        if(i < element.rows.length){
+          element.rows[i].cells[0].innerHTML = json[i-1].studentID
+          element.rows[i].cells[1].innerHTML = json[i-1].name
+          element.rows[i].cells[2].innerHTML = json[i-1].appointment
+          element.rows[i].cells[3].innerHTML = json[i-1].visitTimeLeft
+        } else {
+          let newRow = element.insertRow(i)
+
+          newRow.insertCell(0)
+          newRow.insertCell(1)
+          newRow.insertCell(2)
+          newRow.insertCell(3)
+
+          newRow.cells[0].innerHTML = json[i-1].studentID
+          newRow.cells[1].innerHTML = json[i-1].name
+          newRow.cells[2].innerHTML = json[i-1].appointment
+          newRow.cells[3].innerHTML = json[i-1].visitTimeLeft
+
+          let modifyButton = document.createElement("button")
+          modifyButton.classList.add("tableBtn")
+          modifyButton.classList.add("edit")
+          modifyButton.innerHTML = "Edit"
+          modifyButton.onclick = function () {
+            input1.value = newRow.cells[0].innerHTML
+            input2.value = newRow.cells[1].innerHTML
+            input3.value = newRow.cells[2].innerHTML
+            editFlag = i;
+          }
+          let deleteButton = document.createElement("button")
+          deleteButton.classList.add("tableBtn")
+          deleteButton.classList.add("delete")
+          deleteButton.innerHTML = "Delete"
+          deleteButton.onclick = function () {
+            let body = "DELETE" + i
+            fetch( '/submit', {
+              method:'POST',
+              body 
+            })
+            element.deleteRow(i)
+          }
+          newRow.appendChild(modifyButton)
+          newRow.appendChild(deleteButton)
+        }
+      }
+    })
+    return false
+  }
+
+  window.onload = function() {
+    const button = document.querySelector( 'button' )
+    if(button !== null)
+      button.onclick = submit
+
+    let body = "GETDATA"
+    fetch( '/submit', {
+      method:'POST',
+      body 
+    })
+    .then( response => response.json() )
+    .then( function ( json ) {
+      // do something with the reponse
+      const element = document.querySelector('#taskTable')
+      
+      for(let i = 1; i <= json.length; i++){
+        if(i < element.rows.length){
+          element.rows[i].cells[0].innerHTML = json[i-1].studentID
+          element.rows[i].cells[1].innerHTML = json[i-1].name
+          element.rows[i].cells[2].innerHTML = json[i-1].appointment
+          element.rows[i].cells[3].innerHTML = json[i-1].visitTimeLeft
+        } else {
+          let newRow = element.insertRow(i)
+
+          newRow.insertCell(0)
+          newRow.insertCell(1)
+          newRow.insertCell(2)
+          newRow.insertCell(3)
+
+          newRow.cells[0].innerHTML = json[i-1].studentID
+          newRow.cells[1].innerHTML = json[i-1].name
+          newRow.cells[2].innerHTML = json[i-1].appointment
+          newRow.cells[3].innerHTML = json[i-1].visitTimeLeft
+
+          if(document.title === "CS4241 Assignment 2"){
+            let modifyButton = document.createElement("button")
+            modifyButton.classList.add("tableBtn")
+            modifyButton.classList.add("edit")
+            modifyButton.innerHTML = "Edit"
+            modifyButton.onclick = function () {
+              input1.value = newRow.cells[0].innerHTML
+              input2.value = newRow.cells[1].innerHTML
+              input3.value = newRow.cells[2].innerHTML
+              editFlag = i;
+            }
+            let deleteButton = document.createElement("button")
+            deleteButton.classList.add("tableBtn")
+            deleteButton.classList.add("delete")
+            deleteButton.innerHTML = "Delete"
+            deleteButton.onclick = function () {
+              let body = "DELETE" + i
+              fetch( '/submit', {
+                method:'POST',
+                body 
+              })
+              element.deleteRow(i)
+            }
+            newRow.appendChild(modifyButton)
+            newRow.appendChild(deleteButton)
+          }
+        }
+      }
+
+    })
+  }
--- server.improved.js
+++ server.improved.js
@@ -1,3 +1,5 @@
+//const { getDefaultSettings } = require('http2')
+
 const http = require( 'http' ),
       fs   = require( 'fs' ),
       // IMPORTANT: you must run `npm install` in the directory for this assignment
@@ -7,9 +9,11 @@ const http = require( 'http' ),
       port = 3000
 
 const appdata = [
+  /*
   { 'model': 'toyota', 'year': 1999, 'mpg': 23 },
   { 'model': 'honda', 'year': 2004, 'mpg': 30 },
   { 'model': 'ford', 'year': 1987, 'mpg': 14} 
+  */
 ]
 
 const server = http.createServer( function( request,response ) {
@@ -38,35 +42,85 @@ const handlePost = function( request, response ) {
   })
 
   request.on( 'end', function() {
-    console.log( JSON.parse( dataString ) )
-
-    // ... do something with the data here!!!
+    
+    
+     // ... do something with the data here!!!
+    if(dataString.slice(0,4) === "EDIT")
+    {
+      let rowNum = parseInt(dataString.slice(4,5))
+      dataString = dataString.slice(5)
+      appdata[rowNum-1] = JSON.parse( dataString )
+    } 
+    else if(dataString.slice(0,6) === "DELETE")
+    {
+      let rowNum = parseInt(dataString.slice(6,7))
+      appdata.splice(rowNum - 1, 1)
+    } 
+    else if (dataString === "GETDATA"){
+    } 
+    else 
+    {
+      appdata.push(JSON.parse( dataString ))
+    }
+    appdata.forEach( element => appointmentDate(element))
 
     response.writeHead( 200, "OK", {'Content-Type': 'text/plain' })
-    response.end()
+    //JSON.stringify(appdata[appdata.length-1])
+    response.end(JSON.stringify( appdata ))
   })
 }
 
 const sendFile = function( response, filename ) {
-   const type = mime.getType( filename ) 
-
+const type = mime.getType( filename ) 
    fs.readFile( filename, function( err, content ) {
-
-     // if the error = null, then we've loaded the file successfully
      if( err === null ) {
-
-       // status code: https://httpstatuses.com
        response.writeHeader( 200, { 'Content-Type': type })
        response.end( content )
-
      }else{
-
-       // file not found, error code 404
        response.writeHeader( 404 )
        response.end( '404 Error: File Not Found' )
 
      }
    })
+}
+
+const appointmentDate = function(data){
+  let start = new Date()
+          year = start.getFullYear()
+          month = start.getMonth() + 1
+          date = start.getDate()
+          hour = start.getHours()
+          minute = start.getMinutes()
+          dueYear = parseInt(data.appointment.slice(0, 4))
+          dueMonth = parseInt(data.appointment.slice(5, 7))
+          dueDay = parseInt(data.appointment.slice(8, 10))
+          dueHour = parseInt(data.appointment.slice(11, 13))
+          dueMinute = parseInt(data.appointment.slice(14, 16))
+
+
+  let timeLeft = (((((dueYear - year) * 12 +  dueMonth - month) * 30 + dueDay - date) * 24 + dueHour - hour) * 60 + dueMinute - minute)
+  
+  if(timeLeft < 0){
+    data.visitTimeLeft = "Visit is over"
+  } else {
+
+      let minutesLeft = timeLeft % 60
+          hoursLeft = Math.floor(timeLeft / 60) % 24
+          daysLeft = Math.floor(timeLeft / 60 / 24) % 30
+          monthsLeft = Math.floor(timeLeft / 60 / 24 / 30) % 12
+          yearsLeft =  Math.floor(timeLeft / 60 / 24 / 30 / 12)
+          dueString = "Time Remaining: "
+
+      dueString += yearsLeft > 0 ? yearsLeft + " years " : ""
+      dueString += monthsLeft > 0 ? monthsLeft + " months " : ""
+      dueString += daysLeft > 0 ? daysLeft + " days " : ""
+      dueString += hoursLeft > 0 ? hoursLeft + " hours " : ""
+      dueString += minutesLeft > 0 ? minutesLeft + " minutes" : ""
+
+      
+    data.visitTimeLeft = dueString
+  }
+  
 }
 
 server.listen( process.env.PORT || port )
