a679c1a5b20e0b519ac69247e8229d72f7dfb505
Dooober <cooperdean2002@gmail.com>
2022-09-07 01:07:12 -0400

    Frontend

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -1,3 +1,43 @@
-// Add some Javascript code here, to run on the front end.
+const form = document.getElementById('form')
+form.addEventListener("submit", submit)
 
-console.log("Welcome to assignment 2!")
+async function submit( e ) {
+    e.preventDefault()
+
+    const formElement = e.currentTarget;
+        url = e.action;
+
+    try {
+        const formData = new FormData(formElement)
+
+        const responseData = await postFormDataAsJSON({url, formData})
+
+        console.log({responseData})
+    } catch(error) {
+        console.error(error)
+    }
+
+}
+
+async function postFormDataAsJSON({url, formData}) {
+    const plainFormData = Object.fromEntries(formData.entries())
+        body = JSON.stringify(plainFormData)
+
+    const response = await fetch( url, {
+        method:'POST',
+        headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json"
+        },
+        body: body
+      })
+
+    if (!response.ok) {
+        const errorMessage = await response.text()
+        throw new Error(errorMessage)
+    }
+
+    console.log(response)
+    
+    return response
+}
