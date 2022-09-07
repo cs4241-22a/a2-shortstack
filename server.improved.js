f60d7eb50017665f9c476a0c70e2d6c0137f4d20
Patrick Salisbury <patrickjsalisbury@gmail.com>
2022-09-07 02:42:41 -0400

    user can add/remove data into a table

--- server.improved.js
+++ server.improved.js
@@ -6,11 +6,7 @@ const http = require( 'http' ),
       dir  = 'public/',
       port = 3000
 
-const appdata = [
-  { 'model': 'toyota', 'year': 1999, 'mpg': 23 },
-  { 'model': 'honda', 'year': 2004, 'mpg': 30 },
-  { 'model': 'ford', 'year': 1987, 'mpg': 14} 
-]
+let appdata = []
 
 const server = http.createServer( function( request,response ) {
   if( request.method === 'GET' ) {
@@ -38,12 +34,18 @@ const handlePost = function( request, response ) {
   })
 
   request.on( 'end', function() {
-    console.log( JSON.parse( dataString ) )
+    let data = JSON.parse( dataString )
 
-    // ... do something with the data here!!!
+    if(data.name !== '') {
+      //add new data to appdata if not a clear call
+      appdata.push( data )
+    } else {
+      //clear data
+      appdata = []
+    }
 
     response.writeHead( 200, "OK", {'Content-Type': 'text/plain' })
-    response.end()
+    response.end( JSON.stringify( appdata ) )
   })
 }
 
