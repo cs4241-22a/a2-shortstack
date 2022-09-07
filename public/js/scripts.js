c8b53ed029a6df055d443489d936f3274b5c6d04
hyoder <hryoder@wpi.edu>
2022-09-06 22:27:10 -0400

    init commit

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -1,3 +1,36 @@
-// Add some Javascript code here, to run on the front end.
+const submit = function( e ) {
+    // prevent default form action from being carried out
+    e.preventDefault()
 
-console.log("Welcome to assignment 2!")
+    const title = document.getElementById( 'title' ),
+          genre = document.getElementById( 'genre' ),
+          year  = document.getElementById( 'year'  );
+          json = { title: title.value,
+                   genre: genre.value,
+                   year:   year.value },
+          body = JSON.stringify( json );
+
+    fetch( '/submit', {
+      method:'POST',
+      body 
+    })
+    .then( response => response.json() )
+    .then( json => {
+        build( json );
+    });
+    return false;
+  }
+
+  const build = function( json )
+  {
+    const data = document.getElementById('datatable');
+    data.innerHTML = '<tr><th>title</th><th>genre</th><th>year</th></tr>';
+    json.forEach( entry => {
+        table.innerHTML += '<tr><th>${entry.name}</th><th>${entry.genre}</th><th>${entry.year}</th></tr>';
+    });
+  }
+
+  window.onload = function() {
+    const button = document.querySelector( 'button' )
+    button.onclick = submit
+  }
--- server.improved.js
+++ server.improved.js
@@ -4,12 +4,29 @@ const http = require( 'http' ),
       // to install the mime library used in the following line of code
       mime = require( 'mime' ),
       dir  = 'public/',
-      port = 3000
+      port = 3000;
 
 const appdata = [
-  { 'model': 'toyota', 'year': 1999, 'mpg': 23 },
-  { 'model': 'honda', 'year': 2004, 'mpg': 30 },
-  { 'model': 'ford', 'year': 1987, 'mpg': 14} 
+  { 'title': 'Halloween',
+    'genre': 'horror',
+    'year': 1978
+  },
+  { 'title': 'Final Destination 2',
+    'genre': 'horror',
+    'year': 2003
+  },
+  { 'title': 'A Nightmare On Elm Street 3: Dream Warriors',
+    'genre': 'horror',
+    'year': 1987
+  },
+  { 'title': 'Alvin and the Chipmunks: The Road Chip',
+    'genre': 'family',
+    'year': 2015
+  },
+  { 'title': 'Fast Five',
+    'genre': 'action',
+    'year': 2011
+  }
 ]
 
 const server = http.createServer( function( request,response ) {
@@ -20,32 +37,41 @@ const server = http.createServer( function( request,response ) {
   }
 })
 
-const handleGet = function( request, response ) {
-  const filename = dir + request.url.slice( 1 ) 
+const handleGet = function( request, response )
+{
+  const filename = dir + request.url.slice( 1 );
+  if ( request.url === '/' ) { sendFile( response, 'public/index.html' ); }
+  else { sendFile( response, filename ); }
+};
 
-  if( request.url === '/' ) {
-    sendFile( response, 'public/index.html' )
-  }else{
-    sendFile( response, filename )
-  }
-}
-
-const handlePost = function( request, response ) {
-  let dataString = ''
-
-  request.on( 'data', function( data ) {
-      dataString += data 
-  })
-
-  request.on( 'end', function() {
-    console.log( JSON.parse( dataString ) )
-
+const handlePost = function( request, response )
+{
+  let dataString = '';
+  request.on( 'data', function( data ) { dataString += data; });
+  request.on( 'end', function()
+  {
+    console.log( JSON.parse( dataString ) );
+    movie = JSON.parse( dataString );
+    let toddCheck = checkIfToddPhillips( movie.title, movie.year );
+    movie.toddPhillips = toddCheck;
     // ... do something with the data here!!!
+    response.writeHead( 200, "OK", {'Content-Type': 'text/plain' });
+    response.end();
+  });
+};
 
-    response.writeHead( 200, "OK", {'Content-Type': 'text/plain' })
-    response.end()
-  })
-}
+const checkIfToddPhillips = function( title, year )
+{
+  const years = [ 2000, 2003, 2004, 2006, 2009, 2010, 2011, 2013, 2016, 2019 ];
+  const titles = [ 'road trip', 'old school', 'starsky & hutch',
+                   'school for scoundrels', 'the hangover',
+                   'due date', 'the hangover part ii',
+                   'the hangover part iii', 'war dogs', 'joker' ];
+  const numFilms = 10;
+  let i;
+  for (i = 0 ;  i < numFilms ; i++ ) { if( year = years[i] && title.toLowerCase().equals(titles[i]) ) { return true; } }
+  return false;
+;}
 
 const sendFile = function( response, filename ) {
    const type = mime.getType( filename ) 
