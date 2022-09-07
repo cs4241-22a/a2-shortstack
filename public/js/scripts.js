a62c9d7b26a25597676cc24908e373cec32a7f66
r2pen2 <joedobbelaar@gmail.com>
2022-09-07 15:15:01 -0400

    Homepage is a little messed.

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -1,3 +1,21 @@
-// Add some Javascript code here, to run on the front end.
+window.onload = () => {
+    // Do opening animation...
+    const titlePage = document.getElementById('title-page');
+    const getStartedButton = document.getElementById("get-started-button");
+    titlePage.classList.remove("hidden");
+    setTimeout(() => {
+        getStartedButton.classList.remove("hidden");
+    }, 1000);
 
-console.log("Welcome to assignment 2!")
+    // Add get-started button functionality
+    getStartedButton.onclick = () => {
+        titlePage.classList.add("hidden");
+        setUpMainPage();
+    }
+}
+
+function setUpMainPage() {
+    const mainPage = document.getElementById('main-page');
+    mainPage.classList.remove("hidden");
+    console.log("Setting up main page...");
+}
