cab78a7a916a8302aa82146c1a28a3f58eb7eefb
Glitch (a2-rluu1) <none>
2022-09-07 16:59:10 +0000

    🐭🕝 Updated with Glitch

--- server.improved.js
+++ server.improved.js
@@ -6,11 +6,7 @@ const http = require( 'http' ),
       dir  = 'public/',
       port = 3000
 
-const appdata = [
-  { 'model': 'toyota', 'year': 1999, 'mpg': 23 },
-  { 'model': 'honda', 'year': 2004, 'mpg': 30 },
-  { 'model': 'ford', 'year': 1987, 'mpg': 14} 
-]
+const appdata = []
 
 const server = http.createServer( function( request,response ) {
   if( request.method === 'GET' ) {
@@ -26,7 +22,8 @@ const handleGet = function( request, response ) {
   if( request.url === '/' ) {
     sendFile( response, 'public/index.html' )
   }else{
-    sendFile( response, filename )
+    const html =
+      sendFile( response, filename )
   }
 }
 
@@ -43,7 +40,7 @@ const handlePost = function( request, response ) {
     // ... do something with the data here!!!
 
     response.writeHead( 200, "OK", {'Content-Type': 'text/plain' })
-    response.end()
+    response.end( JSON.stringify( appdata ))
   })
 }
 
