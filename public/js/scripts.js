cb4e35f2764a9108417432ecfd636d07e354d04a
hyoder <hryoder@wpi.edu>
2022-09-07 15:52:14 -0400

    commit 9

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -1,51 +1,37 @@
 const submitter = function( e ) {
-    // prevent default form action from being carried out
     e.preventDefault();
     const title = document.getElementById( 'title' ),
           genre = document.getElementById( 'genre' ),
-          year  = document.getElementById( 'year'  );
-          json = { title: title.value,
-                   genre: genre.value,
-                   year:   year.value },
-          body = JSON.stringify( json );
-    fetch( '/submit', { method:'POST', body })
-    .then( response => response.json() )
-    .then( json => { builder( json ); });
+          year  = document.getElementById( 'year'  ),
+          json  = { title: title.value, genre: genre.value, year: year.value },
+          body  = JSON.stringify( json );
+    fetch( '/submit', { method:'POST', body }).then( response => response.json() ).then( json => { builder( json ); });
     return false; };
-
-const getter = function() {
-    fetch( '/get', { method:'GET', }).then( response => response.json() )
-    .then(json => { builder( json ); }); };
-
+const getter = function() { fetch( '/get', { method:'GET', }).then( response => response.json() ).then(json => { builder( json ); }); };
 const builder = function( json ) {
-    json.forEach(movie=> {
-        const datatable = document.getElementById('datatable');
-        var newrow = datatable.insertRow(datatable.rows.length);
-        var col1 = newrow.insertCell(0);
-        var col2 = newrow.insertCell(1);
-        var col3 = newrow.insertCell(2);
-        var col4 = newrow.insertCell(3);
-        col1.innerHTML = movie.title;
-        col2.innerHTML = movie.genre;
-        col3.innerHTML = movie.year;
-        col4.innerHTML = toddcheck(movie.title, movie.year);
-}); };
-
+    const datatable = document.getElementById('datatable');
+    datatable.innerHTML = '<tr><th>title</th><th>genre</th><th>year</th><th>directed by Todd Phillips?</th></th>';
+    var index = 1;
+    json.forEach(entry => {
+        var newrow = datatable.insertRow(index),
+            col1 = newrow.insertCell(0),
+            col2 = newrow.insertCell(1),
+            col3 = newrow.insertCell(2),
+            col4 = newrow.insertCell(3);
+        col1.innerHTML = entry.title;
+        col2.innerHTML = entry.genre;
+        col3.innerHTML = entry.year;
+        col4.innerHTML = toddcheck( entry.title, entry.year );
+        console.log(index);
+        index++; }); };
 const toddcheck = function( title, year ) {
     const years = [ 2000, 2003, 2004, 2006, 2009, 2010, 2011, 2013, 2016, 2019 ];
-    const titles = [ 'road trip', 'old school', 'starsky & hutch',
-                     'school for scoundrels', 'the hangover',
-                     'due date', 'the hangover part ii',
-                     'the hangover part iii', 'war dogs', 'joker' ];
+    const titles = [ 'road trip', 'old school', 'starsky & hutch', 'school for scoundrels', 'the hangover',
+                     'due date', 'the hangover part ii', 'the hangover part iii', 'war dogs', 'joker' ];
     const numFilms = 10;
-    let i;
-    for (i = 0 ;  i < numFilms ; i++ ) { if( year = years[i] && title.toString().toLowerCase().equals(titles[i]) ) { return true; } }
-    return false; };
-
-    //const data = document.getElementById('datatable');
-    //data.innerHTML = '<tr><th>title</th><th>genre</th><th>year</th></tr>';
-    //json.forEach( entry => { table.innerHTML += '<tr><th>${entry.name}</th><th>${entry.genre}</th><th>${entry.year}</th></tr>'; }); };
-
+    title = title.toString().toLowerCase();
+    for (let i = 0 ;  i < numFilms ; i++ ) { if( year = years[i] && title == titles[i] ) { return 'YES!'; } }
+    return 'NO!'; };
 window.onload = function() {
     const button = document.getElementById( 'submit' );
     button.onclick = submitter;
--- server.improved.js
+++ server.improved.js
@@ -1,54 +1,26 @@
-const http = require( 'http' ),
-      fs   = require( 'fs' ),
-      // IMPORTANT: you must run `npm install` in the directory for this assignment
-      // to install the mime library used in the following line of code
-      mime = require( 'mime' ),
-      dir  = 'public/',
-      port = 3000;
-
-const appdata = [
-  { 'title': 'Halloween', 'genre': 'horror', 'year': 1978 },
-  { 'title': 'War Dogs',  'genre': 'comedy', 'year': 2016 },
-  { 'title': 'A Nightmare On Elm Street 3: Dream Warriors', 'genre': 'horror', 'year': 1987 },
-  { 'title': 'Alvin and the Chipmunks: The Road Chip', 'genre': 'family', 'year': 2015 },
-  { 'title': 'Old School', 'genre': 'comedy', 'year': 2003 }
-]
-
+const http = require( 'http' ), fs   = require( 'fs' ), mime = require( 'mime' ), dir  = 'public/', port = 3000;
+const appdata = [ { 'title': 'Halloween',                                   'genre': 'horror', 'year': 1978 },
+                  { 'title': 'War Dogs',                                    'genre': 'comedy', 'year': 2016 },
+                  { 'title': 'A Nightmare On Elm Street 3: Dream Warriors', 'genre': 'horror', 'year': 1987 },
+                  { 'title': 'Alvin and the Chipmunks: The Road Chip',      'genre': 'family', 'year': 2015 },
+                  { 'title': 'Old School',                                  'genre': 'comedy', 'year': 2003 } ];
 const server = http.createServer( function( request,response ) {
-  if( request.method === 'GET' ) { handleGet( request, response ); }
-  else if( request.method === 'POST' ) { handlePost( request, response ); }});
-
+       if( request.method === 'GET'  ) {  handleGet( request, response ); }
+  else if( request.method === 'POST' ) { handlePost( request, response ); } } );
 const handleGet = function( request, response ) {
   const filename = dir + request.url.slice( 1 );
   if ( request.url === '/' ) { sendFile( response, 'public/index.html' ); }
   else if ( request.url === '/get' ) {
     response.writeHead( 200, 'OK', { 'Content-Type': 'text/plain' });
-    response.end( JSON.stringify(appdata) ); }
+    response.end( JSON.stringify( appdata ) ); }
   else { sendFile( response, filename ); } };
-
 const handlePost = function( request, response ) {
   let dataString = '';
   request.on( 'data', function( data ) { dataString += data; });
   request.on( 'end', function() {
-    movie = JSON.parse( dataString );
-    let toddCheck = checkIfToddPhillips( movie.title, movie.year );
-    movie.toddPhillips = toddCheck;
-    appdata.push(movie);
+    appdata.push( JSON.parse( dataString ) );
     response.writeHead( 200, "OK", {'Content-Type': 'text/plain' });
-    response.end();
-  }); };
-
-const checkIfToddPhillips = function( title, year ) {
-  const years = [ 2000, 2003, 2004, 2006, 2009, 2010, 2011, 2013, 2016, 2019 ];
-  const titles = [ 'road trip', 'old school', 'starsky & hutch',
-                   'school for scoundrels', 'the hangover',
-                   'due date', 'the hangover part ii',
-                   'the hangover part iii', 'war dogs', 'joker' ];
-  const numFilms = 10;
-  let i;
-  for (i = 0 ;  i < numFilms ; i++ ) { if( year = years[i] && title.toString().toLowerCase().equals(titles[i]) ) { return true; } }
-  return false; };
-
+    response.end(); } ); };
 const sendFile = function( response, filename ) {
   const type = mime.getType( filename );
   fs.readFile( filename, function( err, content ) {
@@ -60,5 +32,5 @@ const sendFile = function( response, filename ) {
     else {
       // file not found, error code 404
       response.writeHeader( 404 );
-      response.end( '404 Error: File Not Found' ); } }); };
+      response.end( '404 Error: File Not Found' ); } } ); };
 server.listen( process.env.PORT || port );
