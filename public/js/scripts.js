c649a8253be54cb5623e3b840e183257db6e03a6
Ben Sakac <101744805+benSakac-WPI@users.noreply.github.com>
2022-09-07 21:13:13 -0400

    added rudimentary form validation

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -2,29 +2,36 @@ const submit = function (e) {
 	// prevent default form action from being carried out
 	e.preventDefault();
 	let form = document.querySelector("form");
-	let submission = getForm();
-	form.reset();
-	let body = JSON.stringify(submission);
+	if (checkFrmValid()) {
+		let submission = getForm();
+		form.reset();
+		let body = JSON.stringify(submission);
 
-	fetch("/submit", {
-		method: "POST",
-		body,
-	})
-		.then((response) => response.json())
-		.then((json) => console.log(json));
+		fetch("/submit", {
+			method: "POST",
+			body,
+		})
+			.then((response) => response.json())
+			.then((json) => console.log(json));
 
-	renderTable();
-	document.querySelector("table").scrollIntoView();
+		renderTable();
+		document.querySelector("table").scrollIntoView();
+	} else {
+		alert(
+			"All fields are required. Please fill out all fields before clicking submit."
+		);
+	}
 
 	return false;
 };
 
 window.onload = function () {
-	const button = document.querySelector("button");
+	const button = document.querySelector("#submit");
 	button.onclick = submit;
 	renderTable();
 };
 
+//I'm sure this could be implemented in a much more elegant way but I don't have enough programming experience
 function renderTable() {
 	fetch("/birthdays", {
 		method: "GET",
@@ -63,6 +70,7 @@ function renderTable() {
 					let row = document.createElement("tr");
 					row.className = "results";
 					resultsTable.appendChild(row);
+					
 					let cell1 = row.insertCell(0);
 					let cell2 = row.insertCell(1);
 					let cell3 = row.insertCell(2);
@@ -71,14 +79,15 @@ function renderTable() {
 					let cell6 = row.insertCell(5);
 					let cell7 = row.insertCell(6);
 
-					let delBtn = cell6.appendChild(document.createElement("button"));
-					let modBtn = cell7.appendChild(document.createElement("button"));
 
 					cell1.innerHTML = `${appdata[entries].firstName}`;
 					cell2.innerHTML = `${appdata[entries].lastName}`;
 					cell3.innerHTML = `${appdata[entries].birthday}`;
 					cell4.innerHTML = `${appdata[entries].daysUntil}`;
 					cell5.innerHTML = `${appdata[entries].giftIdea}`;
+
+					let delBtn = cell6.appendChild(document.createElement("button"));
+					let modBtn = cell7.appendChild(document.createElement("button"));
 
 					delBtn.className = `${appdata[entries].submitTime}`;
 					delBtn.innerHTML = "Delete";
@@ -109,20 +118,26 @@ function remove(UID) {
 }
 
 function modify(UID) {
-	let modification = getForm();
 	let form = document.querySelector("form");
-	form.reset();
-	let oldUID = { oldUID: `${UID}` };
-	let bodyObjects = [oldUID, modification];
-	let body = JSON.stringify(bodyObjects);
-	fetch("/modify", {
-		method: "POST",
-		body,
-	})
-		.then((response) => response.json())
-		.then((json) => console.log(json));
+	if (checkFrmValid()) {
+		let modification = getForm();
+		form.reset();
+		let oldUID = { oldUID: `${UID}` };
+		let bodyObjects = [oldUID, modification];
+		let body = JSON.stringify(bodyObjects);
+		fetch("/modify", {
+			method: "POST",
+			body,
+		})
+			.then((response) => response.json())
+			.then((json) => console.log(json));
 
-	renderTable();
+		renderTable();
+	} else {
+		alert(
+			"All fields are required. Please fill out all fields before clicking submit."
+		);
+	}
 }
 
 function convertToObj(a, b) {
@@ -147,7 +162,6 @@ function getForm() {
 		let fieldName = ID.slice(1); //JSON object key
 		formLabels.push(fieldName);
 		formInputs.push(input.value);
-		
 	}
 	let table = document.querySelector("table");
 	table.focus();
@@ -155,3 +169,21 @@ function getForm() {
 	return json;
 }
 
+function checkFrmValid() {
+	let inputIDs = ["#firstName", "#lastName", "#birthday", "#giftIdea"]; //Element IDs for each input field
+	let valid = [];
+	for (let index = 0; index < inputIDs.length; index++) {
+		let ID = inputIDs[index];
+		input = document.querySelector(ID);
+		if (input.value === "") {
+			valid.push(false);
+		} else {
+			valid.push(true);
+		}
+	}
+	if (valid.every((value) => value === true)) {
+		return true;
+	} else {
+		return false;
+	}
+}
