502f0672e0bc5edb204809cb06c767758829525a
meganletendre7 <65192801+meganletendre7@users.noreply.github.com>
2022-09-07 22:16:35 -0400

    Update scripts.js

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -1,3 +1,31 @@
 // Add some Javascript code here, to run on the front end.
 
-console.log("Welcome to assignment 2!")
+
+
+//intended to act as the POST request
+const submit = function (e) {
+  e.preventDefault();
+  
+  const task = document.getElementById('task'),
+        date_created = document.getElementById('date_created'),
+        task_type = document.getElementById('task_type'),
+        date = new Date(),
+        json = {task:task.value, date_created: date, task_type: task_type.value},
+        body = JSON.stringify(json);
+  
+  fetch('/submit', {
+    method: 'POST',
+    body
+  }).then(async response => {
+    renderTable(await response,json())
+  }).catch((reason) => {
+    console.log(reason)
+  })
+  
+  task.value = ''
+  date
+        
+}
+
+//I would need to do something to get the table to pop-up
+//something with adding the HTML and getting it to pop-up with each input
