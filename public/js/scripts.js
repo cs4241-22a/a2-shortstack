a4fcfb9ba21da3aad4e17c901e87f5b79cc11d31
Orina006 <44687580+capricieuxV@users.noreply.github.com>
2022-09-08 17:39:41 -0400

    fixed system issue

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -1,3 +1,195 @@
 // Add some Javascript code here, to run on the front end.
 
 console.log("Welcome to assignment 2!")
+var body;
+
+const text_constructor = function( e ) {
+    e.preventDefault()
+
+    const newMsg = {
+        From: document.getElementById( 'from' ).value,
+        To: document.getElementById( 'to' ).value,
+        Content: document.getElementById( 'content' ).value,
+    }
+
+    if ( isValid( newMsg ) )  {
+        body = JSON.stringify( newMsg )
+
+        fetch( "/send", {
+            method:"POST",
+            body
+        })
+            .then( function( response ) {
+                document.getElementById( 'msg_sent' ).style.display = "flex"
+                document.getElementById( 'msg_sent' ).innerHTML += `<div id="Outbox">
+          <div id="notification">
+          <h1 id="notification2">Congrats, ${newMsg.from}, your message is sucessfully sent!</h1>
+          </div> `
+                document.getElementById( 'Outbox' ).style.display = "none"
+                document.getElementById( 'notification' ).style.display = "none"
+                console.log( response )
+            })
+    }
+    return false
+}
+
+const isValid = function( newMsg ) {
+    if ( newMsg.From =='' || newMsg.From.length > 16 ) {
+        alert( "Please Enter a Name within 16 characters!" )
+        return false
+    }
+    else if ( isNaN( newMsg.To ) || newMsg.To == ''){
+        alert( "Please enter a valid receiver!" )
+        return false
+    }
+    else if ( isNaN( newMsg.To ) || newMsg.Content =='') {
+        alert( "Message cannot be empty" )
+        return false
+    }
+    else
+        return true
+}
+
+const deleteMsg = function( number ){
+    const charNum = { charNum : number }
+    const body = JSON.stringify( charNum )
+
+    fetch('/delete', {
+        method: 'POST',
+        body
+    })
+
+    viewMsg()
+
+    return false
+}
+
+
+const viewMsg = function() {
+
+    document.getElementById( 'text_table' ).style.display = "block"
+    document.getElementById( 'Outbox' ).style.display = "none"
+    document.getElementById( 'notification' ).style.display = "none"
+    document.getElementById( 'Editor' ).style.display = "none"
+    document.getElementById( 'text_sent' ).style.display = "none"
+    document.getElementById( 'text_table' ).innerHTML = "<div id='table-content'><tr>"
+        + "<th>From</th>"
+        + "<th>To</th>"
+        + "<th>Content</th>"
+        + "<th></th>"
+        + "</tr>"
+
+    const character =  fetch('/Inbox', {
+        method: 'GET'
+    }).then(resp => resp.json()).then(
+        data => { console.log(data)
+            //for (let i = 0; i < data.length; i ++){
+            let num = 0
+            data.forEach(function(char){
+
+                let row = "<tr>"
+                    + "<td>" + char.From + "</td>"
+                    + "<td>" + char.To + "</td>"
+                    + "<td>" + char.Content + "</td>"
+                    + `<td> <button class="delBtn" id='delete-character' onclick='deleteMsg( ${num} )'>Delete</button>`
+                    + `<button class="editBtn" id='Editor-button' onclick='displayEdits( ${num} )'>Edit</button> </td>`
+                    + "</tr> </div>"
+
+                document.getElementById( 'text_table' ).innerHTML += row
+                num ++
+            })
+        })
+
+    return false
+}
+
+
+const displayEdits = function( charNum ) {
+
+    const character =  fetch('/Inbox', {
+        method: 'GET'
+    }).then(resp => resp.json()).then(
+        data => { console.log(data)
+            let char = data[charNum]
+            document.getElementById( 'text_table' ).style.display = "none"
+            document.getElementById( 'Outbox' ).style.display = "none"
+            document.getElementById( 'text_sent' ).style.display = "none"
+            document.getElementById( 'Editor' ).style.display = "block"
+            document.getElementById( 'Editor' ).innerHTML = `<div id='editing-character'>
+        <div id="notification">
+      <h1 id="notification3">
+        Typing...
+      </h1>
+      </div>`+`
+        <form action="" method="post">
+            <div id="form-inner2">
+            <div class="sender"><span>From</span> </div>
+            <div>
+              <label for="From">Name:</label>
+              <input type="text" id="senderName" name="sender_name" value=${char.From}>
+            </div>
+            <div>
+              <label for="To">Age:</label>
+              <input type="text" id="receiverName" name="receiver_name" value=${char.To}>
+            </div>
+            <div>
+              <label for="Content">Message:</label>
+              <input type="text" id="msgContent" name="message_content" value=${char.Content}>
+              <button class="action-button" id="Editor-button" onclick="editMsg( ${charNum} )">Edit Messages</button> </div>
+        </form> </div>`
+        })
+    return false
+}
+
+const editMsg = function( number ){
+    const newMsg = {
+        From: document.getElementById( 'from' ).value,
+        To: document.getElementById( 'to' ).value,
+        Content: document.getElementById( 'content' ).value
+        /*From: '1',
+        To: '2,
+        Content: 'test'
+        */
+    }
+
+    if ( isValid(newMsg) ) {
+        debugger
+        const char = { charNum : number, charEdit: newMsg}
+        const body = JSON.stringify( char )
+
+        fetch('/edit', {
+            method: 'POST',
+            body
+        })
+    }
+    location.reload()
+    viewMsg()
+    return false
+}
+
+const mainScreen = function() {
+
+    document.getElementById( 'text_table' ).style.display = "none"
+    document.getElementById( 'Outbox' ).style.display = "flex"
+    document.getElementById( 'text_sent' ).style.display = "none"
+    document.getElementById( 'Editor' ).style.display = "none"
+    location.reload()
+    return false
+}
+
+window.onload = function() {
+    const sendButton = document.getElementById( 'send' )
+    sendButton.onclick = text_constructor
+
+    const outButton = document.getElementById( 'to-character-form' )
+    outButton.onclick = mainScreen
+
+    const inButton = document.getElementById( 'Inbox' )
+    inButton.onclick = viewMsg
+
+    document.getElementById( 'text_table' ).style.display = "none"
+    document.getElementById( 'Outbox' ).style.display = "flex"
+    document.getElementById( 'text_sent' ).style.display = "none"
+    document.getElementById( 'Editor' ).style.display = "none"
+
+}
--- server.improved.js
+++ server.improved.js
@@ -1,72 +1,144 @@
 const http = require( 'http' ),
-      fs   = require( 'fs' ),
-      // IMPORTANT: you must run `npm install` in the directory for this assignment
-      // to install the mime library used in the following line of code
-      mime = require( 'mime' ),
-      dir  = 'public/',
-      port = 3000
+    fs   = require( 'fs' ),
+    // IMPORTANT: you must run `npm install` in the directory for this assignment
+    // to install the mime library used in the following line of code
+    mime = require( 'mime' ),
+    dir  = 'public/',
+    port = 3000
 
 const appdata = [
-  { 'model': 'toyota', 'year': 1999, 'mpg': 23 },
-  { 'model': 'honda', 'year': 2004, 'mpg': 30 },
-  { 'model': 'ford', 'year': 1987, 'mpg': 14} 
+  { 'from': 'Vanessa', 'to': 'Thursday', 'content': 'My cutie!' }
 ]
 
 const server = http.createServer( function( request,response ) {
   if( request.method === 'GET' ) {
-    handleGet( request, response )    
+    handleGet( request, response )
   }else if( request.method === 'POST' ){
-    handlePost( request, response ) 
+    handlePost( request, response )
   }
 })
 
 const handleGet = function( request, response ) {
-  const filename = dir + request.url.slice( 1 ) 
+  const filename = dir + request.url.slice( 1 )
 
   if( request.url === '/' ) {
     sendFile( response, 'public/index.html' )
-  }else{
+  }
+  else if ( request.url === '/view-characters' ){
+    const type = mime.getType( appdata )
+    response.writeHeader( 200, { 'Content-Type': type } )
+    response.write( JSON.stringify( appdata ) )
+    console.log( JSON.stringify( appdata ) )
+    response.end()
+  }
+  else{
     sendFile( response, filename )
   }
 }
 
 const handlePost = function( request, response ) {
+  debugger
   let dataString = ''
 
   request.on( 'data', function( data ) {
-      dataString += data 
+    dataString += data
   })
 
   request.on( 'end', function() {
-    console.log( JSON.parse( dataString ) )
 
-    // ... do something with the data here!!!
+    if ( request.url === '/submit') {
+      let inputData = JSON.parse( dataString )
+      let totalStats = calculateStats(parseInt(inputData.strength), parseInt(inputData.dexterity), parseInt(inputData.intelligence), parseInt(inputData.luck), parseInt(inputData.odd))
+      const newChar = {'name': inputData.name,
+        'age': inputData.age,
+        'occupation': inputData.occupation,
+        'strength': inputData.strength,
+        'dexterity': inputData.dexterity,
+        'intelligence': inputData.intelligence,
+        'luck': inputData.luck,
+        'odd': inputData.odd,
+        'total': totalStats
+      }
+      console.log( newChar )
 
-    response.writeHead( 200, "OK", {'Content-Type': 'text/plain' })
-    response.end()
+      appdata.push( newChar )
+      response.writeHead( 200, "OK", {'Content-Type': 'text/plain' })
+      response.end()
+
+    }
+    else if ( request.url === '/delete') {
+      let inputData = JSON.parse( dataString )
+      let index = inputData.charNum
+
+      //console.log( "Attempting to delete: " + inputData )
+
+      if ( index > -1 ) {
+        appdata.splice( index, 1 )
+      }
+
+      response.writeHead( 200, "OK", {'Content-Type': 'text/plain' })
+      response.end()
+
+    }
+    else if ( request.url === '/edit') {
+      debugger
+      let inputData = JSON.parse( dataString )
+      console.log(inputData)
+      let index = inputData.charNum
+      console.log(index)
+      let totalStats = calculateStats(parseInt(inputData.charEdit.strength), parseInt(inputData.charEdit.dexterity), parseInt(inputData.charEdit.intelligence), parseInt(inputData.charEdit.luck), parseInt(inputData.charEdit.odd))
+      const edited = {'name': inputData.charEdit.name,
+        'age': inputData.charEdit.age,
+        'occupation': inputData.charEdit.occupation,
+        'strength': inputData.charEdit.strength,
+        'dexterity': inputData.charEdit.dexterity,
+        'intelligence': inputData.charEdit.intelligence,
+        'luck': inputData.charEdit.luck,
+        'odd': inputData.charEdit.odd,
+        'total': totalStats
+      }
+      console.log(edited)
+      if ( index > -1 ) {
+        appdata.splice(index, 1, edited)
+        console.log(appdata)
+      }
+      response.writeHead( 200, "OK", {'Content-Type': 'text/plain' })
+      response.end()
+
+    }
+    else
+        // nothing happens
+      response.end()
+
   })
 }
 
+const calculateStats = function (str, dex, int, luk, odd) {
+
+  return str + dex + int + luk + odd
+
+}
+
 const sendFile = function( response, filename ) {
-   const type = mime.getType( filename ) 
+  const type = mime.getType( filename )
 
-   fs.readFile( filename, function( err, content ) {
+  fs.readFile( filename, function( err, content ) {
 
-     // if the error = null, then we've loaded the file successfully
-     if( err === null ) {
+    // if the error = null, then we've loaded the file successfully
+    if( err === null ) {
 
-       // status code: https://httpstatuses.com
-       response.writeHeader( 200, { 'Content-Type': type })
-       response.end( content )
+      // status code: https://httpstatuses.com
+      response.writeHeader( 200, { 'Content-Type': type })
+      response.end( content )
 
-     }else{
+    }else{
 
-       // file not found, error code 404
-       response.writeHeader( 404 )
-       response.end( '404 Error: File Not Found' )
+      // file not found, error code 404
+      response.writeHeader( 404 )
+      response.end( '404 Error: File Not Found' )
 
-     }
-   })
+    }
+  })
 }
 
 server.listen( process.env.PORT || port )
