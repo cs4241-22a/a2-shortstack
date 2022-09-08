6cf6dd5cf4de894e1bd1d76dd966c084f41e11ee
may <may.s.dong@gmail.com>
2022-09-07 23:34:47 -0400

    commit2

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -1,15 +1,16 @@
-// Add some Javascript code here, to run on the front end.
-
 console.log("Welcome to assignment 2!")
 
 const do_submit = function(e) {
-  console.log("submit");
+  const title = document.querySelector('#todo_item'),
+        assigned_date = document.querySelector("#todo_item_assigndate"),
+        due_date = document.querySelector("#todo_item_duedate")
+        json = { 
+          "title": title.value, 
+          "assigned_date": assigned_date.value, 
+          "due_date": due_date.value},
+        data = JSON.stringify(json)
 
-  const input = document.querySelector('#new_todo_item'),
-          json = { "title": input.value },
-          data = JSON.stringify(json)
-
-  fetch("http://localhost:3000", {
+  fetch("/submit", {
     method: "POST",
     body: data,
     headers: {
@@ -21,15 +22,76 @@ const do_submit = function(e) {
     if (!response.ok) {
       throw new Error(`HTTP error, status = ${response.status}`);
     }
-
     return response.json();
   })
 
-  .then((data) => {
-    const todo_item = document.createElement('li')
-    todo_item.innerText = JSON.stringify(data)
-    document.getElementById("todo_list").appendChild(todo_item);
-  })
+  .then(data => update_page(data))
 
   return false;
+}
+const update_page = function(data) {
+  document.getElementById("todo_list").innerHTML = ""; // removes everything in list
+  let counter = 0; // every todo item gets assigned an id based on their position in the list (0 at top)
+  data.forEach(item => {
+    const todo_item = document.createElement("li");
+    const item_array = JSON.parse(item);
+    set_item_elements(todo_item, item_array);
+    
+    todo_item.appendChild(make_delete_button());
+    todo_item.setAttribute("id", counter);
+    todo_item.setAttribute("class", "priority" + item_array.priority);
+    document.getElementById("todo_list").appendChild(todo_item);
+    counter++;
+  })
+}
+
+const make_delete_button = function() {
+  const button_delete = document.createElement("button");
+  button_delete.setAttribute("class", "delete");
+  button_delete.setAttribute("onclick", "delete_element(this)");
+  button_delete.innerText = "X";
+  return button_delete;
+}
+
+// sets items in array as <p> child elements to the todo_item <li> element
+const set_item_elements = function(todo_item, item_array) {
+  for (let key in item_array) {
+    if (key != "priority") {
+      const todo_element = document.createElement("p");
+      todo_element.innerText = item_array[key];
+      todo_item.appendChild(todo_element);
+    }
+  }
+}
+
+const delete_element = function(e) {
+  fetch("/delete", {
+    method: "POST",
+    body: e.parentNode.getAttribute("id"),
+    headers: {
+      "Content-Type": "application/json"
+    }
+  })
+
+  .then (response => {
+    if (!response.ok) {
+      throw new Error(`HTTP error, status = ${response.status}`);
+    }
+    return response.json();
+  })
+
+  .then(data => update_page(data));
+}
+
+window.onload = function() {
+  fetch("/appdata", {
+    method: "GET"
+  })
+  .then (response => {
+    if (!response.ok) {
+      throw new Error(`HTTP error, status = ${response.status}`);
+    }
+    return response.json();
+  })
+  .then(data => update_page(data));
 }
--- server.improved.js
+++ server.improved.js
@@ -6,9 +6,7 @@ const http = require('http'),
       dir  = 'public/',
       port = 3000
 
-const appdata = [
-  {"title": "item 1"}
-]
+const appdata = [];
 
 const server = http.createServer(function(req, res) {
   if (req.method === "GET") {
@@ -24,21 +22,35 @@ const handleGet = function(req, res) {
   if (req.url === "/") {
     sendFile(res, dir + "/index.html");
   }
+  
+  else if (req.url === "/appdata") {
+    res.writeHeader(200, "OK", { "Content-Type": "application/json" });
+    res.end(JSON.stringify(appdata));
+  }
+
   else {
     sendFile(res, dir + req.url);
   }
 }
 
 const handlePost = function(req, res) {
-  console.log("post");
+  let data_string = "";
   req.on("data", function(data) {
-    appdata.push(JSON.parse(data));
+    data_string += data;
+    data_string = generate_priority(data_string);
   })
-
+  
   req.on("end", function() {
+    if (req.url === "/submit") {
+      appdata.push(data_string);
+    }
+    else if (req.url === "/delete") {
+      let index = parseInt(data_string);
+      appdata.splice(index, 1);
+    }
+
     res.writeHeader(200, "OK", { "Content-Type": "application/json" });
-    res.write(JSON.stringify({ currentlist: appdata }));
-    res.end();
+    res.end(JSON.stringify(appdata));
   })
 }
 
@@ -47,8 +59,8 @@ const sendFile = function(res, filename) {
 
   fs.readFile(filename, function(err, content) {
     if (err === null) {
-      res.writeHeader(200, { "Content-Type": type })
-      res.end(content)
+      res.writeHeader(200, { "Content-Type": type });
+      res.end(content);
     }
     
     else {
@@ -58,4 +70,49 @@ const sendFile = function(res, filename) {
   })
 }
 
+/*
+ * generates priority (num between 1-3, higher num = higher urgency) based on how many days it's been
+ * since assignment and how close deadline is
+ * if no deadline, priority = 1
+ * if no assignment, calculate as if it was assigned week before deadline
+ * 
+ * absolutely gives wrong priority if assignment date > due date so don't do that please
+ */
+const generate_priority = function(data_string) {
+  let todo_item = JSON.parse(data_string);
+  let priority = "1";
+  
+  let assigned = todo_item.assigned_date;
+  let due = todo_item.due_date;
+  let date = new Date();
+  if (due) { // if no due date, priority remains 1
+    let due_date = new Date(due);
+    let assigned_date = new Date(due);
+
+    if (!assigned) {
+      // if assigned not set, make it a week before deadline
+      assigned_date.setDate(assigned_date.getDate() - 7); 
+    }
+    else {
+      assigned_date = new Date(assigned);
+    }
+
+    const assigned_due_difference = calculate_days_between(assigned_date, due_date);
+    const assigned_date_difference = calculate_days_between(assigned_date, date);
+
+    const percent_progressed = assigned_date_difference / assigned_due_difference;
+    if (percent_progressed > 0.33 && percent_progressed <= 0.66) { priority = "2"; }
+    else if (percent_progressed > 0.66) { priority = "3" }
+  }
+
+  todo_item.priority = priority;
+  return JSON.stringify(todo_item);
+}
+
+// date2 - date1 (in days)
+const calculate_days_between = function(date1, date2) {
+  return (new Date(date2).getTime() - new Date(date1).getTime()) / (1000 * 3600 * 24);
+}
+
 server.listen(process.env.PORT || port)
+console.log("server up");
