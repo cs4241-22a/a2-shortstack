74db8905fbb442f2fa2dfb65dde897703e3691ba
Siddhartha PRadhan <sppradhan@wpi.edu>
2022-09-07 22:47:13 -0400

    forgot many many many pushes, so commiting everything

--- server.improved.js
+++ server.improved.js
@@ -4,16 +4,26 @@ const http = require( 'http' ),
       // to install the mime library used in the following line of code
       mime = require( 'mime' ),
       port = 3000
-
-const appdata = []
+var taskIdPointer = 0;
+const appdata = [];
 // app data is {"task":"Task Name",   "dueDate": "When the task is due", "taskType":"Work or personal"}
-// self generated: {"taskID" : "Unique ID for a task", "taskCreationDate": "Based on when the request was made", "taskUrgency": "How urgent the Task is, based on (dueDate - taskCreationDate)"}
+// self generated: {"taskID" : "Unique ID for a task", "taskCreationTime": "Based on when the request was made", "taskUrgency": "How urgent the Task is, based on (dueDate - taskCreationDate)"}
 const server = http.createServer( function( request,response ) {
   if( request.method === 'GET' ) {
     handleGet( request, response )    
   }else if( request.method === 'POST' ){
-    handlePost( request, response ) 
-  }
+    console.log('POST REQUEST FOR: ' + request.url.slice(1))
+    if (request.url.slice(1) == "submit") {
+      // task submission
+      handlePost( request, response ) 
+    } else if (request.url.slice(1) == "delete"){
+      //delete
+      handleDelete( request, response )
+    } else if (request.url.slice(1) == "edit"){
+      //edit
+      handleEdit( request, response )
+    }
+  } 
 })
 
 const handleGet = function( request, response ) {
@@ -29,6 +39,64 @@ const handleGet = function( request, response ) {
   }
 }
 
+const handleDelete = function( request, response ) {
+  let dataString = ''
+
+  request.on( 'data', function( data ) {
+      dataString += data 
+  })
+  request.on( 'end', function() {
+    var found = false;
+    const incomingData = JSON.parse(dataString);
+    for (let aTask in appdata){
+      let taskJson = appdata[aTask];
+      if (taskJson.taskID == incomingData.taskID) {
+        found = true;
+        appdata.splice(aTask,1);
+        response.writeHead( 200, "OK", {'Content-Type': 'text/plain' })
+        response.end()
+        break;
+      }
+    }
+    if (!found){
+      response.writeHead( 410, "Gone", {'Content-Type': 'text/plain' })
+      response.end()
+    }})
+}
+
+const handleEdit = function( request, response ) {
+  let dataString = ''
+  request.on( 'data', function( data ) {
+      dataString += data 
+  })
+  request.on( 'end', function() {
+    var found = false;
+    const incomingData = JSON.parse(dataString);
+    var currentDateTime = incomingData.taskCreationTime;
+    if(incomingData.dueDate == null){
+      incomingData.taskUrgency = null;
+    } else {
+      if (currentDateTime > incomingData.dueDate) currentDateTime = incomingData.dueDate;
+      incomingData.taskUrgency = getTaskUrgency(incomingData.dueDate - currentDateTime);
+    }
+    
+    for (let aTask in appdata){
+      let taskJson = appdata[aTask];
+      if (taskJson.taskID == incomingData.taskID) {
+        found = true;
+        appdata.splice(aTask,1);
+        appdata.push(incomingData);
+        response.writeHead( 200, "OK", {'Content-Type': 'text/plain' })
+        response.end()
+        break;
+      }
+    }
+    if (!found){
+      response.writeHead( 410, "Gone", {'Content-Type': 'text/plain' })
+      response.end()
+    }})
+}
+
 const handlePost = function( request, response ) {
   let dataString = ''
 
@@ -37,12 +105,18 @@ const handlePost = function( request, response ) {
   })
 
   request.on( 'end', function() {
+    taskIdPointer++;
+    var currentDateTime = Date.now();
     const incomingData = JSON.parse(dataString);
-    console.log(incomingData.dueDate);
-    // TODO: need to convert dueDate to epoch time
-    var dataRow = incomingData;
-    dataRow.taskCreationDate = Date.now();
-    //dataRow.taskUrgency = getTaskUrgency(dataRow.dueDate, dataRow.taskCreationDate);
+    if(incomingData.dueDate == null){
+      incomingData.taskUrgency = null;
+    } else {
+      if (currentDateTime > incomingData.dueDate) currentDateTime = incomingData.dueDate;
+      console.log("Time received: " +  incomingData.dueDate);
+      incomingData.taskUrgency = getTaskUrgency(incomingData.dueDate - currentDateTime);
+    }
+    incomingData.taskCreationTime = currentDateTime;
+    incomingData.taskID = taskIdPointer;
     // need some sort of check before adding it
     appdata.push(incomingData);
     response.writeHead( 200, "OK", {'Content-Type': 'text/plain' })
@@ -50,7 +124,21 @@ const handlePost = function( request, response ) {
   })
 }
 
-
+// deltaTime in ms
+// Urgency is either 0(past due), 1 (URGENT), 2(medium urgency), 3 (not really)
+const getTaskUrgency = function(deltaTime){
+  const numDaysLeft = deltaTime/86400000;
+  console.log("Days left " + numDaysLeft);
+  if (numDaysLeft <= 0){
+    return 0;
+  }  else if (numDaysLeft <= 2){
+    return 1;
+  } else if (numDaysLeft <= 7){
+    return 2;
+  } else {
+    return 3;
+  }
+}
 const sendTasks = function(response) {
   response.writeHeader( 200, { 'Content-Type': 'application/json' })
   response.end( JSON.stringify(appdata) )
