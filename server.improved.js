6c39f596038d280f914ec1a5f2cddbaa90a1b8a6
Jacob van Steyn <jacobvs2@gmail.com>
2022-09-08 11:48:53 -0400

    Add files

--- server.improved.js
+++ server.improved.js
@@ -1,69 +1,95 @@
 const http = require( 'http' ),
-      fs   = require( 'fs' ),
-      // IMPORTANT: you must run `npm install` in the directory for this assignment
-      // to install the mime library used in the following line of code
-      mime = require( 'mime' ),
-      dir  = 'public/',
-      port = 3000
+    fs   = require( 'fs' ),
+    // IMPORTANT: you must run `npm install` in the directory for this assignment
+    // to install the mime library used in the following line of code
+    mime = require( 'mime' ),
+    dir  = 'public/',
+    port = 3000
 
 const appdata = []
 
 const server = http.createServer( function( request,response ) {
   if( request.method === 'GET' ) {
-    handleGet( request, response )    
+    handleGet( request, response )
   }else if( request.method === 'POST' ){
-    handlePost( request, response ) 
+    handlePost( request, response )
   }
 })
 
 const handleGet = function( request, response ) {
-  const filename = dir + request.url.slice( 1 ) 
+  const filename = dir + request.url.slice( 1 )
 
   if( request.url === '/' ) {
     sendFile( response, 'public/index.html' )
-  }else{
+  } else if (request.url === '/getdata'){
+    response.writeHead(200, "OK", { "Content-Type": "text/plain" });
+    response.write(JSON.stringify(appdata));
+    response.end();
+  }
+  else {
     sendFile( response, filename )
   }
 }
 
 const handlePost = function( request, response ) {
-  let dataString = ''
+  console.log(request.url)
+  if (request.url === '/submit/'){
+    let dataString = ''
 
-  request.on( 'data', function( data ) {
-      dataString += data 
-  })
+    request.on( 'data', function( data ) {
+      dataString += data
+    })
 
-  request.on( 'end', function() {
-    var data = JSON.parse( dataString );
-    console.log("Received Data: " + data);
-    appdata.push(data)
-    
-    response.writeHead(200, "OK", {'Content-Type': 'text/plain' })
-    response.json(appdata)
-    response.end()
-  })
+    request.on( 'end', function() {
+      var data = JSON.parse( dataString );
+      console.log(data);
+      appdata.push(data)
+
+      response.writeHead(200, "OK", {'Content-Type': 'text/plain' })
+      response.write(JSON.stringify(appdata))
+      response.end()
+    })
+  }
+  else if (request.url === '/delete/'){
+    let dataString = ''
+
+    request.on( 'data', function( data ) {
+      dataString += data
+    })
+
+    request.on( 'end', function() {
+      var data = JSON.parse( dataString );
+      console.log(data);
+      delete appdata[data.id]
+
+      response.writeHead(200, "OK", {'Content-Type': 'text/plain' })
+      response.write(JSON.stringify(appdata))
+      response.end()
+    })
+  }
+
 }
 
 const sendFile = function( response, filename ) {
-   const type = mime.getType( filename ) 
+  const type = mime.getType( filename )
 
-   fs.readFile( filename, function( err, content ) {
+  fs.readFile( filename, function( err, content ) {
 
-     // if the error = null, then we've loaded the file successfully
-     if( err === null ) {
+    // if the error = null, then we've loaded the file successfully
+    if( err === null ) {
 
-       // status code: https://httpstatuses.com
-       response.writeHeader( 200, { 'Content-Type': type })
-       response.end( content )
+      // status code: https://httpstatuses.com
+      response.writeHeader( 200, { 'Content-Type': type })
+      response.end( content )
 
-     }else{
+    }else{
 
-       // file not found, error code 404
-       response.writeHeader( 404 )
-       response.end( '404 Error: File Not Found' )
+      // file not found, error code 404
+      response.writeHeader( 404 )
+      response.end( '404 Error: File Not Found' )
 
-     }
-   })
+    }
+  })
 }
 
 server.listen( process.env.PORT || port )
