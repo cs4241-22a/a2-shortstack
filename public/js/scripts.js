2b08695605fa7808dd3014c91c8c2b632922d8e6
Glitch (a2-nkuchipudi) <none>
2022-09-08 04:49:58 +0000

    ⚽🎎 Updated with Glitch

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -1,3 +1,129 @@
-// Add some Javascript code here, to run on the front end.
+const load = function () {
+  fetch("/load", {
+    method: "GET",
+  })
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (appdata) {
+      for (let index = 0; index < appdata.length; index++) {
+        append(appdata, index);
+      }
+    });
+};
 
-console.log("Welcome to assignment 2!")
+const submit = function (e) {
+  
+  e.preventDefault();
+
+  //get the inputs
+  const bookName = document.querySelector("#bookName"),
+    authorName = document.querySelector("#authorName"),
+    totalPages = document.querySelector("#totalPages"),
+    pagesRead = document.querySelector("#pagesRead");
+  
+  //checking if the inputs are valid
+   if(bookName.value === "" || authorName.value === "" || totalPages.value === "" || pagesRead.value === ""){
+    alert("Please enter values for all fields!");
+    document.getElementById("bookForm").reset();
+    return;
+  }
+  
+  //if valid make a json to add
+  const json = {
+      bookName: bookName.value,
+      authorName: authorName.value,
+      totalPages: totalPages.value,
+      pagesRead: pagesRead.value,
+    },
+    body = JSON.stringify(json);
+
+  //fetch method
+  fetch("/submit", {
+    method: "POST",
+    body,
+  })
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (appdata) {
+      console.log(appdata);
+      append(appdata, appdata.length - 1);
+    });
+};
+
+// Remove entry from server data array and update page
+const remove = function (e) {
+  e.preventDefault();
+
+  const element = this.index,
+    json = {
+      index: element,
+    },
+    body = JSON.stringify(json);
+
+  fetch("/remove", {
+    method: "POST",
+    body,
+  })
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (appdata) {
+      let tbody = document.querySelector("#bookshelf-body");
+      tbody.innerHTML = "";
+      for (let index = 0; index < appdata.length; index++) {
+        append(appdata, index);
+      }
+    });
+};
+
+const append = function (data, row) {
+  let tbody = document.querySelector("#bookshelf-body");
+  let newRow = document.createElement("tr");
+  newRow.setAttribute("class", "bookshelf-tr");
+  for (let col = 0; col < 6; col++) {
+    let newCol = document.createElement("td");
+    if (col != 5 && col != 6) {
+      newCol.setAttribute("class", "bookshelf-td");
+    } else {
+      newCol.setAttribute("class", "bookshelf-td-button");
+    }
+    let newElement;
+    switch (col) {
+      case 0:
+        newElement = document.createTextNode(data[row].bookName);
+        break;
+      case 1:
+        newElement = document.createTextNode(data[row].authorName);
+        break;
+      case 2:
+        newElement = document.createTextNode(data[row].totalPages);
+        break;
+      case 3:
+        newElement = document.createTextNode(data[row].pagesRead);
+        break;
+      case 4:
+        newElement = document.createTextNode(data[row].pagesLeft);
+        break;
+      case 5:
+        newElement = document.createElement("button");
+        newElement.innerHTML = "X";
+        newElement.setAttribute("class", "button");
+        newElement.className = "deleteButton";
+        newElement.setAttribute("id", "remove");
+        newElement.onclick = remove;
+        newElement.index = row;
+        break;
+    }
+    newCol.appendChild(newElement);
+    newRow.appendChild(newCol);
+  }
+  tbody.appendChild(newRow);
+};
+
+window.onload = function () {
+  load();
+  const button = document.querySelector("button");
+  button.onclick = submit;
+};
--- server.improved.js
+++ server.improved.js
@@ -1,72 +1,86 @@
 const http = require( 'http' ),
       fs   = require( 'fs' ),
-      // IMPORTANT: you must run `npm install` in the directory for this assignment
-      // to install the mime library used in the following line of code
       mime = require( 'mime' ),
       dir  = 'public/',
       port = 3000
 
+//some example data!!
 const appdata = [
-  { 'model': 'toyota', 'year': 1999, 'mpg': 23 },
-  { 'model': 'honda', 'year': 2004, 'mpg': 30 },
-  { 'model': 'ford', 'year': 1987, 'mpg': 14} 
-]
+  {
+    bookName: "A Dance with Dragons",
+    authorName: "George R.R. Martin",
+    totalPages: "150",
+    pagesRead: "10",
+    pagesLeft: "140",
+  },
+  {
+    bookName: "Moby Dick",
+    authorName: "Herman Melville",
+    totalPages: "200",
+    pagesRead: "10",
+    pagesLeft: "190",
+  },
+];
 
-const server = http.createServer( function( request,response ) {
-  if( request.method === 'GET' ) {
-    handleGet( request, response )    
-  }else if( request.method === 'POST' ){
-    handlePost( request, response ) 
+//creating server
+const server = http.createServer(function (request, response) {
+  if (request.method === "GET") {
+    handleGet(request, response);
+  } else if (request.method === "POST") {
+    handlePost(request, response);
   }
-})
+});
 
-const handleGet = function( request, response ) {
-  const filename = dir + request.url.slice( 1 ) 
+//getting data
+const handleGet = function (request, response) {
+  const filename = dir + request.url.slice(1);
 
-  if( request.url === '/' ) {
-    sendFile( response, 'public/index.html' )
-  }else{
-    sendFile( response, filename )
+  if (request.url === "/") {
+    sendFile(response, "public/index.html");
+  } else if (request.url === "/load") {
+    const type = mime.getType(appdata);
+    response.writeHeader(200, { "Content-Type": type });
+    response.end(JSON.stringify(appdata));
+  } else {
+    sendFile(response, filename);
   }
-}
+};
 
-const handlePost = function( request, response ) {
-  let dataString = ''
+//delete, edit, submit
+const handlePost = function (request, response) {
+  let dataString = "";
+  request.on("data", function (data) {
+    dataString += data;
+  });
 
-  request.on( 'data', function( data ) {
-      dataString += data 
-  })
+  request.on("end", function () {
+    if (request.url === "/submit") {
+      const json = JSON.parse(dataString);
+      json.pagesLeft = json.totalPages - json.pagesRead;
+      appdata.push(json);
+      response.writeHead(200, "OK", { "Content-Type": "text/plain" });
+      response.end(JSON.stringify(appdata));
+    } else if (request.url === "/remove") {
+      const json = JSON.parse(dataString);
+      appdata.splice(json.index, 1);
+      response.writeHead(200, "OK", { "Content-Type": "text/plain" });
+      response.end(JSON.stringify(appdata));
+    } 
+  });
+};
 
-  request.on( 'end', function() {
-    console.log( JSON.parse( dataString ) )
+const sendFile = function (response, filename) {
+  const type = mime.getType(filename);
 
-    // ... do something with the data here!!!
-
-    response.writeHead( 200, "OK", {'Content-Type': 'text/plain' })
-    response.end()
-  })
-}
-
-const sendFile = function( response, filename ) {
-   const type = mime.getType( filename ) 
-
-   fs.readFile( filename, function( err, content ) {
-
-     // if the error = null, then we've loaded the file successfully
-     if( err === null ) {
-
-       // status code: https://httpstatuses.com
-       response.writeHeader( 200, { 'Content-Type': type })
-       response.end( content )
-
-     }else{
-
-       // file not found, error code 404
-       response.writeHeader( 404 )
-       response.end( '404 Error: File Not Found' )
-
-     }
-   })
-}
+  fs.readFile(filename, function (err, content) {
+    if (err === null) {
+      response.writeHeader(200, { "Content-Type": type });
+      response.end(content);
+    } else {
+      response.writeHeader(404);
+      response.end("404 Error: File Not Found");
+    }
+  });
+};
 
 server.listen( process.env.PORT || port )
