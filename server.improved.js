79253b9476deb11a6c9463c4a405c5d82d96048e
meganletendre7 <65192801+meganletendre7@users.noreply.github.com>
2022-09-07 22:15:36 -0400

    Update server.improved.js

--- server.improved.js
+++ server.improved.js
@@ -7,15 +7,25 @@ const http = require( 'http' ),
       port = 3000
 
 const appdata = [
-  { 'model': 'toyota', 'year': 1999, 'mpg': 23 },
-  { 'model': 'honda', 'year': 2004, 'mpg': 30 },
-  { 'model': 'ford', 'year': 1987, 'mpg': 14} 
+  
+  {
+    'task': 'Submit Webware A2', 'date_created': '2022-09-06T13:33', 'task_type': 'School', priority_lvl: 'Low'
+  },
+  
+  {
+    'task': 'Give boss work availability', 'date_created': '2022-09-01T11:16', 'task_type': 'Work', priority_lvl: 'Low'
+  },
+  
+  {
+    'task': "Schedule dentist appointment", 'date_created': '2022-09-03T11:45', 'task_type': 'Personal', priority_lvl: 'Low'
+  }
+
 ]
 
 const server = http.createServer( function( request,response ) {
   if( request.method === 'GET' ) {
     handleGet( request, response )    
-  }else if( request.method === 'POST' ){
+  } else if( request.method === 'POST' ){
     handlePost( request, response ) 
   }
 })
@@ -25,25 +35,28 @@ const handleGet = function( request, response ) {
 
   if( request.url === '/' ) {
     sendFile( response, 'public/index.html' )
-  }else{
+  }else if (request.url === '/list'){
+    sendData(response)
+  } else{
     sendFile( response, filename )
   }
 }
 
+//handles adding a task
 const handlePost = function( request, response ) {
   let dataString = ''
 
-  request.on( 'data', function( data ) {
+  request.on( 'data', ( data ) => {
       dataString += data 
   })
 
-  request.on( 'end', function() {
-    console.log( JSON.parse( dataString ) )
+  request.on( 'end', () => {
+    let newTask = JSON.parse(dataString)
+    newTask.priority_lvl = priority(newTask.date_created);
+    appdata[appdata.length] = newTask
+    sendData(response)
 
-    // ... do something with the data here!!!
-
-    response.writeHead( 200, "OK", {'Content-Type': 'text/plain' })
-    response.end()
+    
   })
 }
 
@@ -68,5 +81,27 @@ const sendFile = function( response, filename ) {
      }
    })
 }
+
+const sendData = function (response){
+  response.writeHeader( 200, { 'Content-Type': 'application/json' })
+  response.end( JSON.stringify( appdata ) )
+}
+
+const priority = function ( date_created ){
+  const today = Date.now();
+  const elapsedTime = (today - date_created)/1000;
+  
+  const seconds = 86400;
+  
+  if(elapsedTime < seconds * 10) {
+    return 'High'
+  } else if (elapsedTime < seconds * 5) {
+    return 'Medium'
+  } else {
+    return 'Low'
+  }
+}
+
+
 
 server.listen( process.env.PORT || port )
