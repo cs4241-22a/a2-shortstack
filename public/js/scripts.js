a9e1428ca671a175da200572d75ea7fd9e8d3007
Colb131 <cfrechette@wpi.edu>
2022-09-07 22:09:34 -0400

    Finished

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -1,3 +1,103 @@
 // Add some Javascript code here, to run on the front end.
 
 console.log("Welcome to assignment 2!")
+
+const submit = function( e ) {
+    // prevent default form action from being carried out
+    e.preventDefault()
+
+    const name = document.querySelector( '#name' ),
+        grade = document.querySelector( '#grade' ),
+        height = document.querySelector( '#height' ),
+        difficulty = grade.value * height.value,
+        json = { name: name.value, grade: grade.value, height: height.value, difficulty: difficulty },
+        body = JSON.stringify( json )
+
+    name.value = ''
+    grade.value = ''
+    height.value = ''
+
+    fetch( '/submit', {
+        method:'POST',
+        body
+    })
+    .then( function( response ) {
+        console.log( response )
+    }).then(showData)
+
+    return false
+}
+
+
+const remove = function( e ) {
+    // prevent default form action from being carried out
+    e.preventDefault()
+
+    const name = document.querySelector( '#removeName' ),
+        json = { nameToRemove: name.value },
+        body = JSON.stringify( json )
+
+    name.value = ''
+
+    fetch( '/remove', {
+        method:'DELETE',
+        body
+    })
+        .then( function( response ) {
+            console.log( response )
+        })
+        .then(showData)
+
+    return false
+}
+
+const showData = function() {
+    fetch( '/show', {
+        method:'GET'
+    })
+        .then( response => response.json())
+        .then( json => initTable(json))
+
+    return false
+
+}
+
+const initTable = function (json) {
+    console.log("Initializing table")
+
+    // Find a <table> element with id="myTable":
+    var table = document.getElementById("dataTable");
+
+    var rowCount = table.rows.length;
+    for (let i = rowCount - 1; i > 0; i--) {
+        table.deleteRow(i);
+    }
+
+    json.forEach(function(x) {
+        // Create an empty <tr> element and add it to the 1st position of the table:
+        var row = table.insertRow(-1);
+
+// Insert new cells (<td> elements) at the 1st and 2nd position of the "new" <tr> element:
+        var cell1 = row.insertCell(0);
+        var cell2 = row.insertCell(1);
+        var cell3 = row.insertCell(2);
+        var cell4 = row.insertCell(3);
+
+
+// Add some text to the new cells:
+        cell1.innerHTML = x.name;
+        cell2.innerHTML = x.grade;
+        cell3.innerHTML = x.height;
+        cell4.innerHTML = x.difficulty;
+    })
+
+}
+
+window.onload = function() {
+    const submitButton = document.querySelector('#submitButton')
+    submitButton.onclick = submit
+    const removeButton = document.querySelector('#removeButton')
+    removeButton.onclick = remove
+
+    showData()
+}
--- server.improved.js
+++ server.improved.js
@@ -6,17 +6,15 @@ const http = require( 'http' ),
       dir  = 'public/',
       port = 3000
 
-const appdata = [
-  { 'model': 'toyota', 'year': 1999, 'mpg': 23 },
-  { 'model': 'honda', 'year': 2004, 'mpg': 30 },
-  { 'model': 'ford', 'year': 1987, 'mpg': 14} 
-]
+let appdata = []
 
 const server = http.createServer( function( request,response ) {
   if( request.method === 'GET' ) {
     handleGet( request, response )    
   }else if( request.method === 'POST' ){
     handlePost( request, response ) 
+  }else if( request.method === 'DELETE'){
+    handleDelete( request, response )
   }
 })
 
@@ -25,7 +23,11 @@ const handleGet = function( request, response ) {
 
   if( request.url === '/' ) {
     sendFile( response, 'public/index.html' )
-  }else{
+  } else if (request.url === '/show') {
+    response.writeHead( 200, "OK", {'Content-Type': 'text/plain' })
+    response.end(JSON.stringify(appdata))
+  }
+  else{
     sendFile( response, filename )
   }
 }
@@ -40,7 +42,27 @@ const handlePost = function( request, response ) {
   request.on( 'end', function() {
     console.log( JSON.parse( dataString ) )
 
-    // ... do something with the data here!!!
+    appdata.push(JSON.parse( dataString ) )
+
+    response.writeHead( 200, "OK", {'Content-Type': 'text/plain' })
+    response.end()
+  })
+}
+
+const handleDelete = function( request, response ) {
+  let dataString = ''
+
+  request.on( 'data', function( data ) {
+    dataString += data
+  })
+
+  request.on( 'end', function() {
+
+    const body = JSON.parse(dataString)
+
+    console.log( body.nameToRemove )
+    appdata = appdata.filter( entry => body.nameToRemove !== entry.name)
+    console.log( appdata )
 
     response.writeHead( 200, "OK", {'Content-Type': 'text/plain' })
     response.end()
