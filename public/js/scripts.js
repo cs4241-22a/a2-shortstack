901cd7096b091b78b35bd8e774bccd36987a2779
Glitch (a2-lbfoucault) <none>
2022-09-08 15:52:14 +0000

    final update

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -1,17 +1,105 @@
 // Add some Javascript code here, to run on the front end.
 
-console.log("Welcome to assignment 2!")
+window.onload = function () {
+  console.log("Welcome to assignment 2!");
 
-const addToList = function (entry, ts) {
-  // const list = document.getElementById("entryList")
-  // const li = document.createElement("li")
-  // li.appendChild(document.createTextNode(entry))
-  // list.appendChild(li)
+  populateTable();
+
+  const submit_btn = document.getElementById("submit");
+  submit_btn.onclick = submit;
+};
+
+// POST submit entry
+const submit = (e) => {
+  // debugger
+  e.preventDefault();
   
-  const table = document.getElementById("entryTable")
-  let row = table.insertRow(1)
-  let col1 = row.insertCell(0)
-  col1.appendChild(document.createTextNode(entry))
-  let col2 = row.insertCell(1)
-  col2.appendChild(document.createTextNode(ts))
-}
+  const name = document.getElementById("name").value,
+    grade = document.getElementById("grade").value,
+    food = document.getElementById("food").value,
+    activity = document.getElementById("activity").value;
+
+  if (name == undefined || name === "") {
+    alert("Please fill in each category");
+    return;
+  }
+
+  const json = { name: name, grade: grade, activity: activity, food: food },
+    body = JSON.stringify(json);
+
+  fetch("/submit", {
+    method: "POST",
+    body,
+  })
+    .then(async (response) => populateTable(await response.json()))
+    .catch((err) => console.error(err));
+  
+  return false;
+};
+
+// GET appdata
+const populateTable = () => {
+  fetch("/data", {
+    method: "GET",
+  })
+    .then(async (response) => updateData(await response.json()))
+    .catch((err) => console.error(err));
+};
+
+// DELETE remove row
+const deleteRow = (e) => {
+  debugger
+  e.preventDefault();
+  const id = e.target.id;
+
+  fetch("/" + id, {
+    method: "DELETE",
+  })
+    .then(async (response) => updateData(await response.json()))
+    .catch((err) => console.error(err));
+};
+
+const updateData = (json) => {
+  const table = document.getElementById("dataTable");
+  table.innerHTML = "";
+  
+  // add the headers back
+  const headers = table.insertRow(0);
+  headers.innerHTML = `
+  <tr>
+    <th>Name</th>
+    <th>Grade</th>
+    <th>Class of</th>
+    <th>Fav Food</th>
+    <th>Fun Activity</th>
+    <th>Actions</th>
+  </tr>
+  `;
+
+  let i = 1;
+  for (let j of json) {
+    const row = table.insertRow(i);
+    const html = `
+    <td>${j.name}</td>
+    <td>${j.grade}</td>
+    <td>${j.year}</td>
+    <td>${j.food}</td>
+    <td>${j.activity}</td>
+    <td>
+      <button
+        class="del"
+        id="remove${i}"
+      >
+        Remove
+      </button>
+    </td>
+    `;
+    row.innerHTML = html;
+    row.id = `${i}`;
+    row.onclick = deleteRow;
+    i++;
+  }
+  
+};
+
+
--- server.improved.js
+++ server.improved.js
@@ -1,72 +1,107 @@
-const http = require( 'http' ),
-      fs   = require( 'fs' ),
-      // IMPORTANT: you must run `npm install` in the directory for this assignment
-      // to install the mime library used in the following line of code
-      mime = require( 'mime' ),
-      dir  = 'public/',
-      port = 3000
+const http = require("http"),
+  fs = require("fs"),
+  mime = require("mime"),
+  dir = "public/",
+  port = 3000;
 
+// initial data
 const appdata = [
-  { 'model': 'toyota', 'year': 1999, 'mpg': 23 },
-  { 'model': 'honda', 'year': 2004, 'mpg': 30 },
-  { 'model': 'ford', 'year': 1987, 'mpg': 14} 
-]
+  {
+    name: "Liliana",
+    grade: "Senior",
+    year: 2023,
+    food: "chicken soup",
+    activity: "volleyball",
+  },
+];
 
-const server = http.createServer( function( request,response ) {
-  if ( request.method === 'GET' ) {
-    handleGet( request, response )    
-  } else if ( request.method === 'POST' ) {
-    handlePost( request, response ) 
+// API
+const server = http.createServer(function (request, response) {
+  if (request.method === "GET") {
+    handleGet(request, response);
+  } else if (request.method === "POST") {
+    handlePost(request, response);
+  } else if (request.method === "DELETE") {
+    handleDelete(request, response);
   }
-})
+});
 
-const handleGet = function( request, response ) {
-  const filename = dir + request.url.slice( 1 ) 
+const handleGet = function (request, response) {
+  const filename = dir + request.url.slice(1);
 
-  if ( request.url === '/' ) {
-    sendFile( response, 'public/index.html' )
+  if (request.url === "/") {
+    sendFile(response, "public/index.html");
+  } else if (request.url === "/data") {
+    response.writeHeader(200, { "Content-type": "application/json" });
+    response.end(JSON.stringify(appdata));
+  } else if (request.url === "/submit") {
+    response.writeHeader(200, { "Content-type": "application/json" });
+    response.end(JSON.stringify(appdata));
   } else {
-    sendFile( response, filename )
+    sendFile(response, filename);
   }
-}
+};
 
-const handlePost = function( request, response ) {
-  let dataString = ''
+const handlePost = function (request, response) {
+  let dataString = "";
 
-  request.on( 'data', function( data ) {
-      dataString += data 
-  })
+  request.on("data", function (data) {
+    dataString += data;
+  });
 
-  request.on( 'end', function() {
-    console.log( JSON.parse( dataString ) )
+  request.on("end", function () {
+    // add entry to appdata
+    let newEntry = JSON.parse(dataString);
+    newEntry.year = getGradYear(newEntry.grade);
+    appdata[appdata.length] = newEntry;
 
-    // ... do something with the data here!!!
+    response.writeHeader(200, { "Content-type": "application/json" });
+    response.end(JSON.stringify(appdata));
+  });
+};
 
-    response.writeHead( 200, "OK", {'Content-Type': 'text/plain' })
-    response.end()
-  })
-}
+const handleDelete = function (request, response) {
+  // remove row from appdata. request url looks like /remove#
+  // debugger
+  const idx = parseInt(request.url.subtstring(7)); 
+  appdata.splice(idx, 1);
+  
+  response.writeHeader(200, { "Content-type": "application/json" });
+  response.end(JSON.stringify(appdata));
+};
 
-const sendFile = function( response, filename ) {
-   const type = mime.getType( filename ) 
+const sendFile = function (response, filename) {
+  const type = mime.getType(filename);
 
-   fs.readFile( filename, function( err, content ) {
-
-     // if the error = null, then we've loaded the file successfully
-     if ( err === null ) {
-
-       // status code: https://httpstatuses.com
-       response.writeHeader( 200, { 'Content-Type': type })
-       response.end( content )
-
+  fs.readFile(filename, function (err, content) {
+    // if the error = null, then we've loaded the file successfully
+    if (err === null) {
+      // status code: https://httpstatuses.com
+      response.writeHeader(200, { "Content-Type": type });
+      response.end(content);
     } else {
+      // file not found, error code 404
+      response.writeHeader(404);
+      response.end("404 Error: File Not Found");
+    }
+  });
+};
 
-       // file not found, error code 404
-       response.writeHeader( 404 )
-       response.end( '404 Error: File Not Found' )
+// get derived field from given grade
+const getGradYear = (grade) => {
+  let year = "";
+  if (grade === "Freshman") {
+    year = 2026;
+  } else if (grade === "Sophomore") {
+    year = 2025;
+  } else if (grade === "Junior") {
+    year = 2024;
+  } else if (grade === "Senior") {
+    year = 2023;
+  } else {
+    year = "";
+  }
+  return year;
+};
 
-     }
-   })
-}
-
-server.listen( process.env.PORT || port )
+server.listen(process.env.PORT || port);
