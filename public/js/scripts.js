85a13864c6fb5a8769ac2df120aa597260f92136
kaelinpanneton <68758878+kaelinpanneton@users.noreply.github.com>
2022-09-07 23:23:04 -0400

    final updates for glitch

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -10,12 +10,18 @@
     let useremail = document.querySelector("#useremail")
     let bookTitle = document.querySelector("#bookTitle")
     let bookAuthor = document.querySelector("#bookAuthor")
+    let currentDate = document.querySelector("#currentDate")
+    let holdID = Math.floor(Math.random() * 11)
+    //let holdID = Array.indexOf(rowid, 0)
     let json = {
         username: username.value,
         useremail: useremail.value,
         bookTitle: bookTitle.value,
         bookAuthor: bookAuthor.value,
-        holdConfirm: ""
+        currentDate: currentDate.value,
+        holdID: holdID,
+        holdConfirm: "",
+        del: false
     }
     let body = JSON.stringify(json)
     fetch( '/submit', {
@@ -36,20 +42,58 @@
         board.innerHTML = ""
 
         formResults.forEach((element, index) => {
-            board.innerHTML +=
-            "<tr><td>" + element.username + " has placed a hold on " 
+            let row = board.insertRow(0)
+            row.id = "row" + element.holdID
+            row.addEventListener("click", function(){ select(row.id); })
+            let cell = row.insertCell(0)
+            cell.innerHTML =
+            element.username + " has placed a hold on " 
             + element.bookTitle + " by " 
-            + element.bookAuthor + ". A confirmation will be sent to " 
-            + element.useremail + "." + "</td></tr>"
+            + element.bookAuthor + " on " 
+            + element.currentDate + ". A confirmation will be sent to " 
+            + element.useremail + ". You are position "
+            + element.holdID + " in the hold queue."
         })
     }
 
-/*     const remove = function( d ) {
-        let index, table = document.getElementById("reserveData")
-        table.deleteRow(index)
-    } */
+    let selectedRow = ""
+
+    const select = function ( rowid ) {
+        selectedRow = rowid
+        console.log(selectedRow)
+    }
+
+    const remove = function( d ) {
+        d.preventDefault()
+        let row = document.getElementById(selectedRow)
+        console.log(row)
+            //row.parentNode.removeChild(row)
+        document.getElementById("reserveData").deleteRow(row)
+    }
+
+    const removeData = function( holdID ) {
+    let json = {
+        holdID: holdID,
+        del: true
+    }
+    let body = JSON.stringify(json)
+    fetch( '/remove', {
+      method:'POST',
+      body
+    })
+    .then( async function ( response ) {
+        let formResults = await response.json()
+        refreshInfo(formResults)
+        console.log( formResults )
+      })
+
+      return false;  
+    }
 
     window.onload = function() {
-        const button = document.querySelector( 'button' )
-        button.onclick = submit
+        const submitbutton = document.getElementById( 'submitButton' )
+        submitbutton.onclick = submit
+
+        const removebutton = document.getElementById( 'removeButton' )
+        removebutton.onclick = remove
     }
--- server.improved.js
+++ server.improved.js
@@ -10,11 +10,18 @@ const appdata = [
 
 ]
 
+//sorts newHold array by ascending date for use in the queueHold
+const sortedHolds = appdata.slice().sort((a,b) => b.currentDate - a.currentDate)
+console.log(sortedHolds)
+
 const server = http.createServer( function( request,response ) {
   if( request.method === 'GET' ) {
-    handleGet( request, response )    
+    handleGet( request, response ) 
   }else if( request.method === 'POST' ){
-    handlePost( request, response ) 
+    if ( request.url === "/submit")
+    handlePostSubmit( request, response ) 
+    if ( request.url === "/remove")
+    handlePostRemove( request, response )
   }
 })
 
@@ -23,12 +30,13 @@ const handleGet = function( request, response ) {
 
   if( request.url === '/' ) {
     sendFile( response, 'public/index.html' )
-  }else{
+  } else{
     sendFile( response, filename )
-  }
+  } 
+
 }
 
-const handlePost = function( request, response ) {
+const handlePostSubmit = function( request, response ) {
   let dataString = ''
 
   request.on( 'data', function( data ) {
@@ -38,8 +46,41 @@ const handlePost = function( request, response ) {
   request.on( 'end', function() {
     console.log( JSON.parse( dataString ) )
     let newHold = JSON.parse( dataString )
-    if (newHold.holdConfirm < 1) {
-      newHold.holdConfirm = ""
+    newHold.holdConfirm = ""
+    
+    console.log( newHold )
+    appdata.push( newHold )
+
+    response.writeHead( 200, "OK", {'Content-Type': 'text/plain' })
+    response.end( JSON.stringify( appdata ) )
+    console.log(appdata)
+    response.end()
+  })
+}
+
+const handlePostRemove = function( request, response ) {
+  let dataString = ''
+
+  request.on( 'data', function( data ) {
+      dataString += data 
+  })
+
+  request.on( 'end', function() {
+    if(newHold.del === true){
+      remove(appdata)
+    }
+  })
+
+  request.on( 'end', function() {
+    console.log( JSON.parse( dataString ) )
+    let newHold = JSON.parse( dataString )
+    newHold.holdConfirm = ""
+    
+    
+    for (let i= 0; i < appdata.length; i++) {
+      if (holdID === appdata[i].holdID)
+        appdata.splice(i, 1)
+        break
     }
 
     console.log( newHold )
@@ -51,6 +92,8 @@ const handlePost = function( request, response ) {
     response.end()
   })
 }
+
+  
 
 const sendFile = function( response, filename ) {
    const type = mime.getType( filename ) 
