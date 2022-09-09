5f6a362e65adcb1a9ab0655c052de89673dce334
Glitch (a2-griffinatchue) <none>
2022-09-09 02:12:43 +0000

    ⛄📤 Updated with Glitch

--- server.improved.js
+++ server.improved.js
@@ -6,10 +6,36 @@ const http = require( 'http' ),
       dir  = 'public/',
       port = 3000
 
+const starters = [
+  { 'pokémon': 'bulbasaur', 'type': "Grass",},
+  { 'pokémon': 'squirtle', 'type': "Water"},
+  { 'pokémon': 'charmander', 'type': "Fire"},
+  { 'pokémon': 'pikachu', 'type': "Electric"},
+  { 'pokémon': 'chikorita', 'type': "Grass",},
+  { 'pokémon': 'totodile', 'type': "Water"},
+  { 'pokémon': 'cyndaquil', 'type': "Fire"},
+  { 'pokémon': 'treeko', 'type': "Grass",},
+  { 'pokémon': 'mudkip', 'type': "Water"},
+  { 'pokémon': 'torchic', 'type': "Fire"},
+  { 'pokémon': 'turtwig', 'type': "Grass",},
+  { 'pokémon': 'piplup', 'type': "Water"},
+  { 'pokémon': 'chimchar', 'type': "Fire"},
+  { 'pokémon': 'snivy', 'type': "Grass",},
+  { 'pokémon': 'oshawott', 'type': "Water"},
+  { 'pokémon': 'tepig', 'type': "Fire"},
+  { 'pokémon': 'chespin', 'type': "Grass",},
+  { 'pokémon': 'froakie', 'type': "Water"},
+  { 'pokémon': 'fennekin', 'type': "Fire"},
+  { 'pokémon': 'rowlet', 'type': "Grass",},
+  { 'pokémon': 'popplio', 'type': "Water"},
+  { 'pokémon': 'litten', 'type': "Fire"},
+  { 'pokémon': 'grookey', 'type': "Grass",},
+  { 'pokémon': 'sobble', 'type': "Water"},
+  { 'pokémon': 'scorbunny', 'type': "Fire"}
+]
+
 const appdata = [
-  { 'model': 'toyota', 'year': 1999, 'mpg': 23 },
-  { 'model': 'honda', 'year': 2004, 'mpg': 30 },
-  { 'model': 'ford', 'year': 1987, 'mpg': 14} 
+  
 ]
 
 const server = http.createServer( function( request,response ) {
@@ -38,12 +64,92 @@ const handlePost = function( request, response ) {
   })
 
   request.on( 'end', function() {
-    console.log( JSON.parse( dataString ) )
-
     // ... do something with the data here!!!
+    let data = JSON.parse( dataString )
+    
+    switch ( data.type.toLowerCase()){
+      case "normal":
+        data.str = "None"
+        data.wk  = "Fighting"
+        break
+      case "fire":
+        data.str = "Grass, Ice, Bug, Steel"
+        data.wk  = "Water, Ground, Rock"
+        break
+        case "water":
+        data.str = "Fire, Ground, Rock"
+        data.wk  = "Electric, Grass"
+        break
+      case "electric":
+        data.str = "Water, Flying"
+        data.wk  = "Ground"
+        break
+      case "grass":
+        data.str = "Water, Ground, Rock"
+        data.wk  = "Fire, Ice, Poison, Flying, Bug"
+        break
+      case "ice":
+        data.str = "Grass, Ground, Flying, Dragon"
+        data.wk  = "Fire, Fighting, Rock, Steel"
+        break
+      case "fighting":
+        data.str = "Normal, Ice, Rock, Dark, Steel"
+        data.wk  = "Flying, Psychic, Fairy"
+        break
+      case "poison":
+        data.str = "Grass, Fairy"
+        data.wk  = "Ground, Psychic"
+        break
+      case "ground":
+        data.str = "Fire, Electric, Poison, Rock, Steel"
+        data.wk  = "Water, Grass, Ice"
+        break
+      case "flying":
+        data.str = "Grass, Fighting, Bug"
+        data.wk  = "Electric, Ice, Rock"
+        break
+      case "psychic":
+        data.str = "Fighting, Poison"
+        data.wk  = "Bug, Ghost, Dark"
+        break
+      case "bug":
+        data.str = "Grass, Psychic, Dark"
+        data.wk  = "Fire, Flying, Rock"
+        break
+      case "rock":
+        data.str = "Fire, Ice, Flying, Bug"
+        data.wk  = "Water, Grass, Fighting, Ground, Steel"
+        break
+      case "ghost":
+        data.str = "Psychic, Ghost"
+        data.wk  = "Ghost, Dark"
+        break
+      case "dragon":
+        data.str = "Dragon"
+        data.wk  = "Ice, Dragon, Fairy"
+        break
+      case "dark":
+        data.str = "Psychic, Ghost"
+        data.wk  = "Fighting, Bug, Fairy"
+        break
+      case "steel":
+        data.str = "Ice, Rock, Fairy"
+        data.wk  = "Fire, Fighting, Ground"
+        break
+      case "fairy":
+        data.str = "Fighting, Dragon, Dark"
+        data.wk  = "Poison, Steel"
+        break
+      default:
+        data.type = "???"
+        data.str  = "???"
+        data.wk   = "???"
+        break
+    }
 
+    appdata.push(JSON.stringify( data ))
     response.writeHead( 200, "OK", {'Content-Type': 'text/plain' })
-    response.end()
+    response.end( JSON.stringify( data ) )
   })
 }
 
