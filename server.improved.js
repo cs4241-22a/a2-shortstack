f0bc949bfb6b15081f173b768ec6e4935467d9f5
Glitch (a2-cindytrac) <none>
2022-09-10 02:52:14 +0000

    🦭🍇 Updated with Glitch

--- server.improved.js
+++ server.improved.js
@@ -1,72 +1,118 @@
-const http = require( 'http' ),
-      fs   = require( 'fs' ),
-      // IMPORTANT: you must run `npm install` in the directory for this assignment
-      // to install the mime library used in the following line of code
-      mime = require( 'mime' ),
-      dir  = 'public/',
-      port = 3000
+const http = require("http"),
+  fs = require("fs"),
+  // IMPORTANT: you must run `npm install` in the directory for this assignment
+  // to install the mime library used in the following line of code
+  mime = require("mime"),
+  dir = "public/",
+  port = 3000;
 
 const appdata = [
-  { 'model': 'toyota', 'year': 1999, 'mpg': 23 },
-  { 'model': 'honda', 'year': 2004, 'mpg': 30 },
-  { 'model': 'ford', 'year': 1987, 'mpg': 14} 
-]
+  {
+    book_title: "Throne of Glass",
+    author_name: "Sarah J. Maas",
+    date_started: "09-01-20",
+    date_comp: "09-04-20",
+    time_took: "3 days",
+  },
+  {
+    book_title: "Throne of Glass",
+    author_name: "Sarah J. Maas",
+    date_started: "09-01-20",
+    date_comp: "09-04-20",
+    time_took: "3 days",
+  },
+];
 
-const server = http.createServer( function( request,response ) {
-  if( request.method === 'GET' ) {
-    handleGet( request, response )    
-  }else if( request.method === 'POST' ){
-    handlePost( request, response ) 
+const server = http.createServer(function (request, response) {
+  if (request.method === "GET") {
+    handleGet(request, response);
+  } else if (request.method === "POST") {
+    handlePost(request, response);
   }
-})
+});
 
-const handleGet = function( request, response ) {
-  const filename = dir + request.url.slice( 1 ) 
+const handleGet = function (request, response) {
+  const filename = dir + request.url.slice(1);
 
-  if( request.url === '/' ) {
-    sendFile( response, 'public/index.html' )
-  }else{
-    sendFile( response, filename )
+  if (request.url === "/") {
+    sendFile(response, "public/index.html");
+  } else if (request.url === "/public/js/scripts.js") {
+    sendFile(response, "public/js/scripts.js");
+  } else if (request.url === "/public/css/style.css") {
+    sendFile(response, "public/css/style.css");
+  } else if (request.url === "/submit") {
+    //getting data from appdata
+    response.writeHeader(200, { "Content-Type": "text/plain" });
+    response.end(JSON.stringify(appdata));
+  } else {
+    sendFile(response, filename);
   }
-}
+};
 
-const handlePost = function( request, response ) {
-  let dataString = ''
+const handlePost = function (request, response) {
+  let dataString = "";
 
-  request.on( 'data', function( data ) {
-      dataString += data 
-  })
+  request.on("data", function (data) {
+    dataString += data;
+  });
 
-  request.on( 'end', function() {
-    console.log( JSON.parse( dataString ) )
-
+  request.on("end", function () {
+    console.log(JSON.parse(dataString));
+    let jsonData = JSON.parse(dataString);
+    console.log(jsonData + "1");
     // ... do something with the data here!!!
+    if (request.url === "/submit") {
+      console.log(jsonData); //debug
+      appdata.push(jsonData); //pushing/adding data to appdata
+    }
+   
+      
+    let index = 0;
+    let toBeRemoved = [];
+    let needToDelete = false;
+    // REMOVES BOOK FROM LIB
+    appdata.forEach((entry) => {
+      if (jsonData.book_title == entry.book_title && jsonData.author_name == entry.author_name) {
+        needToDelete = true;
+        toBeRemoved.push(index);
+        console.log("toBeRemoved: ", toBeRemoved);
+      } 
+      
+      else {
+        index++;
+      }
+    })
+    if (toBeRemoved) {
+      toBeRemoved.forEach((entryToRemove) => {
+        appdata.splice(entryToRemove, 1);
+        console.log(jsonData.book_title + "BOOK");
+        console.log(jsonData + "2");
+      });
+    }
+    
 
-    response.writeHead( 200, "OK", {'Content-Type': 'text/plain' })
-    response.end()
-  })
-}
+    response.writeHead(200, "OK", { "Content-Type": "text/plain" });
+    response.end(JSON.stringify(appdata));
+  });
+};
 
-const sendFile = function( response, filename ) {
-   const type = mime.getType( filename ) 
 
-   fs.readFile( filename, function( err, content ) {
 
-     // if the error = null, then we've loaded the file successfully
-     if( err === null ) {
+const sendFile = function (response, filename) {
+  const type = mime.getType(filename);
 
-       // status code: https://httpstatuses.com
-       response.writeHeader( 200, { 'Content-Type': type })
-       response.end( content )
+  fs.readFile(filename, function (err, content) {
+    // if the error = null, then we've loaded the file successfully
+    if (err === null) {
+      // status code: https://httpstatuses.com
+      response.writeHeader(200, { "Content-Type": type });
+      response.end(content);
+    } else {
+      // file not found, error code 404
+      response.writeHeader(404);
+      response.end("404 Error: File Not Found");
+    }
+  });
+};
 
-     }else{
-
-       // file not found, error code 404
-       response.writeHeader( 404 )
-       response.end( '404 Error: File Not Found' )
-
-     }
-   })
-}
-
-server.listen( process.env.PORT || port )
+server.listen(process.env.PORT || port);
