919b08625bc6a29cf46af6771d7696896fc1b45d
Nappa22428 <Nappa22428@gmail.com>
2022-10-12 23:35:10 -0400

    Add files via upload

--- server.improved.js
+++ server.improved.js
@@ -7,10 +7,19 @@ const http = require( 'http' ),
       port = 3000
 
 const appdata = [
-  { 'model': 'toyota', 'year': 1999, 'mpg': 23 },
-  { 'model': 'honda', 'year': 2004, 'mpg': 30 },
-  { 'model': 'ford', 'year': 1987, 'mpg': 14} 
-]
+  {
+    game: "Tetris",
+    dateBought: "08-15-01",
+    dateCompleted: "08-15-01",
+
+  },
+  {
+    game: "Tetris",
+    dateBought: "08-15-01",
+    dateCompleted: "08-15-01",
+
+  },
+];
 
 const server = http.createServer( function( request,response ) {
   if( request.method === 'GET' ) {
@@ -20,16 +29,27 @@ const server = http.createServer( function( request,response ) {
   }
 })
 
-const handleGet = function( request, response ) {
-  const filename = dir + request.url.slice( 1 ) 
+const handleGet = function (request, response) {
+  const filename = dir + request.url.slice(1);
 
-  if( request.url === '/' ) {
-    sendFile( response, 'public/index.html' )
-  }else{
-    sendFile( response, filename )
-  }
-}
-
+  if (request.url === "/") 
+    {
+      sendFile(response, "public/index.html");
+    } else if (request.url === "/public/js/scripts.js") 
+    {
+      sendFile(response, "public/js/scripts.js");
+    } else if (request.url === "/public/css/style.css") 
+    {
+      sendFile(response, "public/css/style.css");
+    } else if (request.url === "/submit") 
+    {
+      response.writeHeader(200, { "Content-Type": "text/plain" });
+      response.end(JSON.stringify(appdata));
+    } else 
+    {
+      sendFile(response, filename);
+    }
+};
 const handlePost = function( request, response ) {
   let dataString = ''
 
@@ -39,34 +59,33 @@ const handlePost = function( request, response ) {
 
   request.on( 'end', function() {
     console.log( JSON.parse( dataString ) )
+    let jsonData = JSON.parse(dataString);
+    console.log(jsonData + "1");
+    
+     if (request.url === "/submit") {
+      console.log(jsonData);
+      appdata.push(jsonData); 
+    }
+    response.writeHead(200, "OK", { "Content-Type": "text/plain" });
+    response.end(JSON.stringify(appdata));
+  });
+};
 
-    // ... do something with the data here!!!
+const sendFile = function (response, filename) {
+  const type = mime.getType(filename);
 
-    response.writeHead( 200, "OK", {'Content-Type': 'text/plain' })
-    response.end()
-  })
-}
+  fs.readFile(filename, function (err, content) {
+    // if the error = null, then we've loaded the file successfully
+    if (err === null) {
+      // status code: https://httpstatuses.com
+      response.writeHeader(200, { "Content-Type": type });
+      response.end(content);
+    } else {
+      // file not found, error code 404
+      response.writeHeader(404);
+      response.end("404 Error: File Not Found");
+    }
+  });
+};
 
-const sendFile = function( response, filename ) {
-   const type = mime.getType( filename ) 
-
-   fs.readFile( filename, function( err, content ) {
-
-     // if the error = null, then we've loaded the file successfully
-     if( err === null ) {
-
-       // status code: https://httpstatuses.com
-       response.writeHeader( 200, { 'Content-Type': type })
-       response.end( content )
-
-     }else{
-
-       // file not found, error code 404
-       response.writeHeader( 404 )
-       response.end( '404 Error: File Not Found' )
-
-     }
-   })
-}
-
-server.listen( process.env.PORT || port )
+server.listen(process.env.PORT || port);
